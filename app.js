ff2487f97797bb4cd2920e3612867622e058aeef
Isabella Li <dnflyangle@gmail.com>
2015-12-24 11:48:58 +1100

    remove jade, add ejs, remove layout, we will extract it later

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
