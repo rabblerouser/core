0a7e9c732d2f26684b780a24b29ba3aae12cd9d3
Andrew Jones <andrew@andrewjones.id.au>
2016-01-04 09:50:12 +1100

    enable post to members route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var membersController = require('../controllers/membersController');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/members", membersController.newMemberHandler);
 
 module.exports = router;
