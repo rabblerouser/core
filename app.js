a430f0ab7f92717b632edab1df3632e4ec8a4267
Nat Young <nat.young7@gmail.com>
2016-01-04 11:26:22 +1100

    html form to save a member to the database

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'html');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(sassMiddleware({
       src: path.join(__dirname, 'public'),
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/members/new', function(req, res, next) {
+  res.render('members/new', { title: 'New Member' });
+});
+
 router.post("/members", membersController.newMemberHandler);
 
 module.exports = router;
