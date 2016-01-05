0edcb8ed10298fdf889a9ce802bbb5fa224e3f5c
Isabella Li <dnflyangle@gmail.com>
2016-01-05 16:06:12 +1100

    remove the useless routers as we are one page now, HO-RAY!

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,6 @@ router.get('/members/new', function(req, res, next) {
   res.render('members/new', { title: 'New Member' });
 });
 
-router.get('/members/eligibility', function(req, res, next) {
-    res.render('members/eligibility', {title: 'Eligibility'});
-});
-
-router.get('/members/payment', function(req, res, next) {
-    res.render('members/payment', {title: 'Pay What You Want'});
-});
-
 router.post("/members", membersController.newMemberHandler);
 
 module.exports = router;
