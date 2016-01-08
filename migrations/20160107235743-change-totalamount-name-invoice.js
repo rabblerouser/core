a2e52f2f5b89a8ff60d9d2c4a4050cd0f84b37af
Andrew Jones <andrew@andrewjones.id.au>
2016-01-08 11:39:08 +1100

    fix the change totalAmount to totalAmountInCents migration so it waits for the migration to actually finish

--- migrations/20160107235743-change-totalamount-name-invoice.js
+++ migrations/20160107235743-change-totalamount-name-invoice.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
-    queryInterface.renameColumn("Invoices", "totalAmount", "totalAmountInCents")
+    up: function (queryInterface, Sequelize, done) {
+        queryInterface.renameColumn("Invoices", "totalAmount", "totalAmountInCents").nodeify(done);
   },
 
-  down: function (queryInterface, Sequelize) {
-    queryInterface.renameColumn("Invoices", "totalAmountInCents", "totalAmount")
+    down: function (queryInterface, Sequelize, done) {
+        queryInterface.renameColumn("Invoices", "totalAmountInCents", "totalAmount").nodeify(done);
   }
 };
