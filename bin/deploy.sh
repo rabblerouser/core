aa0be409cfc5accd5a103c8b8c722b1335228436
Andrew Jones <andrew@andrewjones.id.au>
2016-01-11 15:43:03 +1100

    read host config in from file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,8 +5,7 @@ set -e
 : "${APP_GIT_SHA:?Expecting APP_GIT_SHA to be set}"
 : "${CERT_EMAIL:?Expecting CERT_EMAIL to be set}"
 : "${DB_PASSWORD:?Expecting DB_PASSWORD to be set}"
-: "${HOST:?Expecting HOST to be set}"
 
 sudo yum install ansible
 
-ansible-playbook --check --diff -i "${HOST}," provisioning/prod.yml
+ansible-playbook --check --diff -i hosts provisioning/prod.yml
