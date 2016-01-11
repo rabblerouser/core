6ea785edec272865291d4e5186d9ab8a9b9e3a40
Andrew Jones <andrew@andrewjones.id.au>
2016-01-11 15:02:38 +1100

    add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+: "${APP_GIT_SHA:?Expecting APP_GIT_SHA to be set}"
+: "${CERT_EMAIL:?Expecting CERT_EMAIL to be set}"
+: "${DB_PASSWORD:?Expecting DB_PASSWORD to be set}"
+: "${HOST:?Expecting HOST to be set}"
+
+sudo yum install ansible
+
+ansible-playbook -i "#{HOST}," provisioning/prod.yml
