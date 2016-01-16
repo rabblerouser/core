1cd76cf987405d50bd6604d8c94cbbcb74b8c0d4
Nat Young <nat.young7@gmail.com>
2016-01-16 23:25:22 +1100

    update html semantics to match other states

--- components/Payment.jsx
+++ components/Payment.jsx
@@ -13,7 +13,7 @@ export default class Payment extends Component {
     }
 
     render() {
-        return <div id="payment-form">
+        return <fieldset>
             <h1>Pay What You Want</h1>
             <div className="form-body">
                 <div className="reminder">
@@ -22,38 +22,33 @@ export default class Payment extends Component {
                         <b>Full Membership</b> of Pirate Party Australia is currently <b>Whatever you want!</b>
                     </div>
                 </div>
-                <div className="contribution">
-                    <div className="heading">
-                        <h2>Membership Contribution</h2>
-                        <i>Please enter a whole dollar value (minimum $0)</i>
-                    </div>
-                    <div className="contribution-amount">
-                        <div className="currency">$AUD</div>
-                        <input type="text" name="totalAmount" id="totalAmount" onChange={this.handleAmountChanged}/>
-                    </div>
+                <div className="heading">
+                    <h2>Membership Contribution</h2>
+                    <i>Please enter a whole dollar value (minimum $0)</i>
                 </div>
-                <div className="payment-method">
-                    <div className="heading">
-                        <h2>Choose a Payment Method</h2>
-                        <i>Pay your selected amount.</i>
-                    </div>
-                    <div className="field-group">
-                        <label>
-                            <input type="radio" name="paymentType" defaultValue="deposit"/>Direct Debit
-                        </label>
-                        <label>
-                            <input type="radio" name="paymentType" defaultValue="cheque"/>Cheque
-                        </label>
-                        <StripePayment email={this.props.email}
-                                       amount={this.state.amount}/>
-                    </div>
+                <div className="contribution-amount">
+                    <div className="currency">$AUD</div>
+                    <input type="text" name="totalAmount" id="totalAmount" onChange={this.handleAmountChanged}/>
+                </div>
+                <div className="heading">
+                    <h2>Choose a Payment Method</h2>
+                    <i>Pay your selected amount.</i>
+                </div>
+                <div className="field-group">
+                    <label>
+                        <input type="radio" name="paymentType" defaultValue="deposit"/>Direct Debit
+                    </label>
+                    <label>
+                        <input type="radio" name="paymentType" defaultValue="cheque"/>Cheque
+                    </label>
+                    <StripePayment email={this.props.email}
+                                   amount={this.state.amount} />
                 </div>
                 <div className="navigation">
-                    <button type="button" className="hidden" id="payment-continue-button">Continue</button>
+                    <button type="button" id="payment-continue-button">Continue</button>
                     <p>or <a id="payment-go-back">go back</a></p>
                 </div>
             </div>
-        </div>;
-
+        </fieldset>;
     }
 }
