c5fe140eac90d24e335430dae53c05b6244e7d59
Isabella Li <dnflyangle@gmail.com>
2016-01-19 11:07:16 +1100

    render warning message properly.

--- components/Errors.jsx
+++ components/Errors.jsx
@@ -3,13 +3,24 @@ import React, {Component} from 'react';
 export default class Errors extends Component {
     constructor(props) {
         super(props);
+        this.getClass = this.getClass.bind(this);
+    }
+
+    getClass(invalidaFields) {
+      return invalidaFields.length > 1 ? "validationErrors" : "hidden";
     }
 
     render() {
-       return <ul className="errors">
-            {this.props.invalidFields.map(function(field){
-                return <li>{field}</li>;
-            })}
-        </ul>
+       return (<div className={this.getClass(this.props.invalidFields)}>
+                 <img src="/images/cross.svg" />
+                 <div className="validationErrors-text">
+                    <span>Please check the following fields:</span>
+                    <ul className="errors">
+                       {this.props.invalidFields.map(function(field){
+                           return <li>{field}</li>;
+                       })}
+                    </ul>
+                </div>
+              </div>)
     }
 }
