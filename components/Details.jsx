f8619112cd8abb0881f9329105244f4be1673130
Chris Maxwell <cmaxwell@MacBook-Pro-2.local>
2016-01-22 15:40:23 +1100

    seperated address errors into individual errors with messages

--- components/Details.jsx
+++ components/Details.jsx
@@ -10,17 +10,50 @@ export default class Details extends Component {
         this.getPersonalInformationSubtitletext = this.getPersonalInformationSubtitletext.bind(this);
         this.getResidentialAddressSubtitleText = this.getResidentialAddressSubtitleText.bind(this);
         this.handlePostalAddress = this.handlePostalAddress.bind(this);
+        this.handleValidationErrors = this.handleValidationErrors.bind(this);
         this.validator = memberValidator;
-        this.state = { invalidFields: [],
-                       showPostalAddress: false };
+        this.state = {
+            invalidFields: [],
+            errorMessages: [],
+            showPostalAddress: false
+        };
     }
 
     handlePostalAddress() {
-      if (this.refs.differentPostal.checked){
-        this.setState( { showPostalAddress: true } );
-      } else {
-        this.setState( { showPostalAddress: false } );
-      }
+        if (this.refs.differentPostal.checked) {
+            this.setState({showPostalAddress: true});
+        } else {
+            this.setState({showPostalAddress: false});
+        }
+    }
+
+    handleValidationErrors(validationErrors) {
+        var errors = [];
+        let errorMessages = {
+            firstName: "First Name: Please enter a valid first name. No numbers or symbols allowed.",
+            lastName: "Last Name: Please enter a valid last name. No numbers or symbols allowed.",
+            dateOfBirth: "Date Of Birth: Date of birth must be in format DD/MM/YYY and be over 16 years of age.",
+            email: "Email: Please enter a valid email address i.e. valid@email.com",
+            primaryPhoneNumber: "Number: Phone number must begin with a '0' or '+61' and be 9-10 digits in length.",
+            secondaryPhoneNumber: "Number: Phone number must begin with a '0' or '+61' and be 9-10 digits in length.",
+            residentialAddress: "Residential Address: Please enter an address",
+            residentialState: "Residential State: Please select a state from the dropdown menu.",
+            residentialCountry: "Residential Country: Please select a country from the dropdown menu.",
+            residentialPostcode: "Residential Postcode: Please enter a postcode. Must be 4 digits in length and using only numbers.",
+            residentialSuburb: "Residential Suburb: Please enter a suburb.",
+            postalAddress: "Postal Address: Please enter an address",
+            postalState: "Postal State: Please select a state from the dropdown menu.",
+            postalCountry: "Postal Country: Please select a country from the dropdown menu.",
+            postalPostcode: "Postal Postcode: Please enter a postcode. Must be 4 digits in length and using only numbers.",
+            postalSuburb: "Postal Suburb: Please enter a suburb."
+        };
+
+        _.forEach(validationErrors, function(error){
+            errors.push(errorMessages[error]);
+        });
+        this.setState({invalidFields: validationErrors});
+        this.setState({errorMessages: errors});
+
     }
 
     componentDidMount() {
@@ -32,17 +65,17 @@ export default class Details extends Component {
     }
 
     getPersonalInformationSubtitletext() {
-      if(this.props.membershipType === "full") {
-        return "Please enter your details exactly as they would appear on the electoral roll.";
-      }
-      return "Please enter your details";
+        if (this.props.membershipType === "full") {
+            return "Please enter your details exactly as they would appear on the electoral roll.";
+        }
+        return "Please enter your details";
     }
 
     getResidentialAddressSubtitleText() {
-      if(this.props.membershipType === "full") {
-        return "Please enter the address that you are enrolled to vote from.";
-      }
-      return "Please enter your address";
+        if (this.props.membershipType === "full") {
+            return "Please enter the address that you are enrolled to vote from.";
+        }
+        return "Please enter your address";
     }
 
 
@@ -71,12 +104,14 @@ export default class Details extends Component {
                 postcode: this.refs.postalPostcode.value
             }
         };
-        if(!this.refs.differentPostal.checked) {
+
+        if (!this.refs.differentPostal.checked) {
             fieldValues.postalAddress = fieldValues.residentialAddress;
         }
-        var invalidFields = this.validator.isValid(fieldValues);
-        if (invalidFields.length > 0) {
-            return this.setState({invalidFields: invalidFields });
+        var validationErrors = this.validator.isValid(fieldValues);
+        if (validationErrors.length > 0) {
+            this.handleValidationErrors(validationErrors);
+            return;
         }
         return this.props.saveAndContinue(fieldValues);
     }
@@ -85,12 +120,17 @@ export default class Details extends Component {
         return (
             <fieldset>
                 <h1 className="form-title">Details</h1>
+
                 <div className="form-body">
-                    <Errors invalidFields={this.state.invalidFields} />
+                    <Errors invalidFields={this.state.errorMessages}/>
+
                     <div className="reminder">
                         <img src="/images/reminder.svg"></img>
+
                         <div className="reminder-text">
-                            The information provided in this form may be used for the purpose of ensuring that the Pirate Party can register or remain registered as a political party in Australia, and its states and territories. <a href="/">View our Privacy Policy.</a>
+                            The information provided in this form may be used for the purpose of ensuring that the
+                            Pirate Party can register or remain registered as a political party in Australia, and its
+                            states and territories. <a href="/">View our Privacy Policy.</a>
                         </div>
                     </div>
                     <div className="heading">
@@ -99,13 +139,17 @@ export default class Details extends Component {
                     </div>
                     <div className="field-group">
                         <label htmlFor="firstName">Given Name(s)*</label>
-                        <input type="text" defaultValue={this.props.formValues.firstName} ref="firstName" id="firstName" className="firstName" />
+                        <input type="text" defaultValue={this.props.formValues.firstName} ref="firstName" id="firstName"
+                               className="firstName" />
                         <label htmlFor="lastName">Surname*</label>
-                        <input type="text" defaultValue={this.props.formValues.lastName} ref="lastName" id="lastName" className="lastName" />
+                        <input type="text" defaultValue={this.props.formValues.lastName} ref="lastName" id="lastName"
+                               className="lastName"/>
                         <label htmlFor="dateOfBirth">Date of Birth*</label>
-                        <input type="text" defaultValue={this.props.formValues.dateOfBirth} ref="dateOfBirth" placeholder="DD/MM/YYYY" id="dateOfBirth" className="dateOfBirth" />
+                        <input type="text" defaultValue={this.props.formValues.dateOfBirth} ref="dateOfBirth"
+                               placeholder="DD/MM/YYYY" id="dateOfBirth" className="dateOfBirth"/>
                         <label htmlFor="gender">Gender</label>
-                        <input type="text" defaultValue={this.props.formValues.gender} ref="gender" id="gender" className="gender" />
+                        <input type="text" defaultValue={this.props.formValues.gender} ref="gender" id="gender"
+                               className="gender"/>
                     </div>
                     <div className="heading">
                         <h2 className="sub-title"> Residential Address</h2>
@@ -117,42 +161,54 @@ export default class Details extends Component {
                         <label htmlFor="residentialAddress[suburb]">Suburb/City*</label>
                         <input type="text" defaultValue={this.props.formValues.residentialAddress.suburb} ref="residentialSuburb" id="residentialAddress[suburb]" className="residentialSuburb" />
                         <label htmlFor="residentialAddress[country]">Country*</label>
-                        <select defaultValue={this.props.formValues.residentialAddress.country} ref="residentialCountry" id="residentialAddress[country]" className="residentialCountry">
+                        <select defaultValue={this.props.formValues.residentialAddress.country} ref="residentialCountry"
+                                id="residentialAddress[country]" className="residentialCountry">
                         </select>
 
                         <div className="state-code">
                             <label htmlFor="residentialAddress[state]">State*</label>
-                            <select defaultValue={this.props.formValues.residentialAddress.state} ref="residentialState" id="residentialAddress[state]" className="residentialState">
+                            <select defaultValue={this.props.formValues.residentialAddress.state} ref="residentialState"
+                                    id="residentialAddress[state]" className="residentialState">
                                 <option value="New South Wales">New South Wales</option>
                             </select>
                             <label htmlFor="residentialAddress[postcode]">Postcode*</label>
-                            <input type="text" defaultValue={this.props.formValues.residentialAddress.postcode} ref="residentialPostcode" id="residentialAddress[postcode]" className="residentialPostcode" />
+                            <input type="text" defaultValue={this.props.formValues.residentialAddress.postcode}
+                                   ref="residentialPostcode" id="residentialAddress[postcode]"
+                                   className="residentialPostcode"/>
                         </div>
                         <label>
-                            <input type="checkbox" onChange={this.handlePostalAddress} defaultValue={this.props.formValues.differentPostal} ref="differentPostal" value="Yes"/>
+                            <input type="checkbox" onChange={this.handlePostalAddress}
+                                   defaultValue={this.props.formValues.differentPostal} ref="differentPostal"
+                                   value="Yes"/>
                             My residential address differs from my postal address.
                         </label>
                     </div>
-                    <div id="postal-address" className={(() => { return this.state.showPostalAddress ? '' : 'hidden';})()}>
+                    <div id="postal-address"
+                         className={(() => { return this.state.showPostalAddress ? '' : 'hidden';})()}>
                         <div className="heading">
                             <h2 className="sub-title"> Postal Address</h2>
                             <i>Please enter the postal address.</i>
                         </div>
                         <div className="field-group">
                             <label htmlFor="postalAddress[address]">Address*</label>
-                            <input type="text" defaultValue={this.props.formValues.postalAddress.address} ref="postalAddress" id="postalAddress" />
+                            <input type="text" defaultValue={this.props.formValues.postalAddress.address}
+                                   ref="postalAddress" id="postalAddress"/>
                             <label htmlFor="postalAddress[suburb]">Suburb*</label>
-                            <input type="text" defaultValue={this.props.formValues.postalAddress.suburb} ref="postalSuburb" id="postalAddress[suburb]" />
+                            <input type="text" defaultValue={this.props.formValues.postalAddress.suburb}
+                                   ref="postalSuburb" id="postalAddress[suburb]"/>
                             <label htmlFor="postalAddress[country]">Country*</label>
-                            <select defaultValue={this.props.formValues.postalAddress.country} ref="postalCountry" id="postalAddress[country]">
+                            <select defaultValue={this.props.formValues.postalAddress.country} ref="postalCountry"
+                                    id="postalAddress[country]">
                             </select>
 
                             <div className="state-code">
                                 <label htmlFor="postalAddress[state]">State*</label>
-                                <select defaultValue={this.props.formValues.postalAddress.state} ref="postalState" id="postalAddress[state]">
+                                <select defaultValue={this.props.formValues.postalAddress.state} ref="postalState"
+                                        id="postalAddress[state]">
                                 </select>
                                 <label htmlFor="postalAddress[postcode]">Postcode*</label>
-                                <input type="text" defaultValue={this.props.formValues.postalAddress.postcode} ref="postalPostcode" id="postalAddress[postcode]" />
+                                <input type="text" defaultValue={this.props.formValues.postalAddress.postcode}
+                                       ref="postalPostcode" id="postalAddress[postcode]"/>
                             </div>
                         </div>
                     </div>
@@ -162,11 +218,14 @@ export default class Details extends Component {
                     </div>
                     <div className="field-group">
                         <label htmlFor="email">Email*</label>
-                        <input type="text" defaultValue={this.props.formValues.email} ref="email" id="email" className="email" />
+                        <input type="text" defaultValue={this.props.formValues.email} ref="email" id="email"
+                               className="email"/>
                         <label htmlFor="phoneNumber">Phone number*</label>
-                        <input type="text" defaultValue={this.props.formValues.primaryPhoneNumber} ref="phoneNumber" id="primaryPhoneNumber" className="primaryPhoneNumber" />
+                        <input type="text" defaultValue={this.props.formValues.primaryPhoneNumber} ref="phoneNumber"
+                               id="primaryPhoneNumber" className="primaryPhoneNumber"/>
                         <label htmlFor="phoneNumber">Secondary Phone</label>
-                        <input type="text" defaultValue={this.props.formValues.secondaryPhone} ref="secondaryPhone" id="secondaryPhone" className="secondaryPhone" />
+                        <input type="text" defaultValue={this.props.formValues.secondaryPhone} ref="secondaryPhone"
+                               id="secondaryPhone" className="secondaryPhone"/>
                     </div>
                     <div className="navigation">
                         <button onClick={this.submitDetails}>Continue</button>
@@ -175,5 +234,7 @@ export default class Details extends Component {
                 </div>
             </fieldset>
         )
-    };
+    }
+
+;
 }
