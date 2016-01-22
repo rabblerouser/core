b2133a965f4b53d1af0005e2087393ded44b6530
Claire Dodd <cdodd@thoughtworks.com>
2016-01-22 16:01:49 +1100

    Minor typo

--- components/declaration-text/OtherMemberDeclarationText.jsx
+++ components/declaration-text/OtherMemberDeclarationText.jsx
@@ -9,7 +9,7 @@ export default class OtherMemberDeclarationText extends Component {
                     <p>I wish to become a member of Pirate Party Australia.
                         I have read and understand the Pirate Party Australia Constitution
                         and agree with its platform and principles,
-                        and will to the best of my ability work to uphold and promote them
+                        and will to the best of my ability work to uphold and promote them.0
                     </p>
                 </div>;
     }
