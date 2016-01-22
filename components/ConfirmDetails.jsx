b6252dc43decafc0f4525fd4ab598d3934552d72
Claire Dodd <cdodd@thoughtworks.com>
2016-01-22 15:56:26 +1100

    Confirm your details page now changes depending on membership type

--- components/ConfirmDetails.jsx
+++ components/ConfirmDetails.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import Errors from './Errors.jsx';
+import FullMemberDeclarationText from './declaration-text/FullMemberDeclarationText.jsx';
+import OtherMemberDeclarationText from './declaration-text/OtherMemberDeclarationText.jsx';
 
 export default class ConfirmDetails extends Component {
     constructor(props) {
         super(props);
         this.getFullAddress = this.getFullAddress.bind(this);
         this.submitMember = this.submitMember.bind(this);
+        this.getDeclaration = this.getDeclaration.bind(this);
         this.state = {
             errors: []
         };
@@ -28,6 +31,13 @@ export default class ConfirmDetails extends Component {
         }
     }
 
+    getDeclaration() {
+      if(this.props.formValues.membershipType === 'full'){
+          return <FullMemberDeclarationText />;
+      }
+      return <OtherMemberDeclarationText />;
+    }
+
     render() {
         return <fieldset>
             <h1 className="form-title">Confirm</h1>
@@ -39,26 +49,12 @@ export default class ConfirmDetails extends Component {
                     <i> Read the following and click the checkbox below.</i>
                 </div>
                 <div className="declaration">
-                    <div className="declaration-text">
-                        <p>I wish to become a member of Pirate Party Australia. I have read and understand the <b><a href="#">Pirate Party
-                        Australia Constitution</a></b> and agree with its platform and principles, and will to the best of my
-                        ability work to uphold and promote them. </p>
-
-                        <p>I am enrolled for Federal elections, i.e. I am 16 years of age or older. I am an Australian
-                        citizen (or a British citizen who was on the Australian Electoral roll on 25 of January 1984)
-                        and I have lived at the above address for at least one month.</p>
-
-                        <p>I consent to this form being forwarded to the Australia Electoral Commission (AEC) in support of
-                        the party’s application for registration as a political party.</p>
-
-                        <b><a href="#">If you are not enrolled to vote, click here.</a></b>
-
-                    </div>
+                  {this.getDeclaration()}
                 </div>
                 <label className="confirmationLabel">
                     <input type="checkbox" name="circumstance" ref="declarationConfirmation"/>
                     I confirm that I am enrolled to vote in federal elections and consent to my information being sent
-                    to the Australia Electoral Commission.*
+                    to the Australia Electoral Commission.<span className="mandatoryField">* </span>
                 </label>
                 <div className="heading">
                     <h2 className="sub-title">Check Your Details</h2>
---
+++ components/declaration-text/OtherMemberDeclarationText.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class OtherMemberDeclarationText extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return  <div className="declaration-text">
+                    <p>I wish to become a member of Pirate Party Australia.
+                        I have read and understand the Pirate Party Australia Constitution
+                        and agree with its platform and principles,
+                        and will to the best of my ability work to uphold and promote them
+                    </p>
+                </div>;
+    }
+}
