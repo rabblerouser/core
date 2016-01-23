5c8326f31d13dbc3fe1e2fd922c2d6b93abaf4d9
Andrew Jones <andrew@andrewjones.id.au>
2016-01-23 12:12:50 +1100

    add uuid to member

--- models/member.js
+++ models/member.js
@@ -1,22 +1,25 @@
 'use strict';
 
+const uuid = require("node-uuid");
+
 module.exports = (sequelize, DataTypes) => {
-  var Member = sequelize.define('Member', {
-    email: {type: DataTypes.STRING, primaryKey: true},
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    primaryPhoneNumber: DataTypes.STRING,
-    secondaryPhoneNumber: DataTypes.STRING,
-    dateOfBirth: DataTypes.DATEONLY,
-    membershipType: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: (models) => {
-        models.Address.hasOne(Member, {foreignKey: "postalAddress"});
-        models.Address.hasOne(Member, {foreignKey: "residentialAddress"});
-      }
-    }
-  });
-  return Member;
+    var Member = sequelize.define('Member', {
+        id: { type: DataTypes.UUID, defaultValue: uuid.v4() },
+        email: { type: DataTypes.STRING, primaryKey: true },
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        gender: DataTypes.STRING,
+        primaryPhoneNumber: DataTypes.STRING,
+        secondaryPhoneNumber: DataTypes.STRING,
+        dateOfBirth: DataTypes.DATEONLY,
+        membershipType: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: (models) => {
+                models.Address.hasOne(Member, {foreignKey: "postalAddress"});
+                models.Address.hasOne(Member, {foreignKey: "residentialAddress"});
+            }
+        }
+    });
+    return Member;
 };
