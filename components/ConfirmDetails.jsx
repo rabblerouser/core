a4bf9a58b31b40f92bca1400da1741fbf2e6873e
Florencia <fleur94@users.noreply.github.com>
2016-01-29 10:31:30 +1100

    MOAR SPACING ISSUES

--- components/ConfirmDetails.jsx
+++ components/ConfirmDetails.jsx
@@ -73,7 +73,7 @@ export default class ConfirmDetails extends Component {
                 </div>
                 <div className="navigation">
                     <button onClick={this.submitMember}>My details are correct</button>
-                    <p>or <a id="go-back" onClick={this.props.previousStep}>go back to change your details</a></p>
+                    <p>or <a onClick={this.props.previousStep}>go back to change your details</a></p>
                 </div>
             </div>
         </fieldset>
