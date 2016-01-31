2d30dd4db9bcfc73a18ee0311f3e82fc5ac85d0a
Isabella Li <dnflyangle@gmail.com>
2016-01-31 16:59:04 +1100

    Add transaction id to invoice table.

--- models/invoice.js
+++ models/invoice.js
@@ -1,19 +1,20 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  var Invoice = sequelize.define('Invoice', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    memberEmail: DataTypes.STRING,
-    totalAmountInCents: DataTypes.BIGINT,
-    paymentDate:  DataTypes.DATEONLY,
-    paymentType: DataTypes.STRING,
-    reference: DataTypes.STRING,
-    paymentStatus: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: (models) => {
-      }
-    }
-  });
-  return Invoice;
+    var Invoice = sequelize.define('Invoice', {
+        id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+        memberEmail: DataTypes.STRING,
+        totalAmountInCents: DataTypes.BIGINT,
+        paymentDate:  DataTypes.DATEONLY,
+        paymentType: DataTypes.STRING,
+        reference: DataTypes.STRING,
+        paymentStatus: DataTypes.STRING,
+        transactionId: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: (models) => {
+            }
+        }
+    });
+    return Invoice;
 };
