353d05a359b22a93b376ec2f9e6a4cae772bcbed
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-02 10:19:44 +1100

    Changin Jasmine reporter so I can debug from the output

--- spec/support/specHelper.js
+++ spec/support/specHelper.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const sinon = require("sinon");
-const Q = require("q");
-require("jasmine-sinon");
+let reporters = require('jasmine-reporters');
+const sinon = require('sinon');
+const Q = require('q');
+require('jasmine-sinon');
 
 var logger = require('../../lib/logger');
 var ChargeCardError = require('../../errors/ChargeCardError');
 
-var models = require("../../models"),
+var models = require('../../models'),
     Address = models.Address,
     Member = models.Member,
     Invoice = models.Invoice,
     AdminUser = models.AdminUser;
 
+var terminalReporter = new reporters.TerminalReporter({
+    verbosity: 3,
+    color: true,
+    showStack: true
+});
+jasmine.getEnv().addReporter(terminalReporter);
 
 beforeEach((done) => {
     Invoice.truncate({cascade: true}).then(() => {
