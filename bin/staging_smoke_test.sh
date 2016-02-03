55f1eb633818cceeb0ae5adb5a76801348edfdda
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-03 15:50:17 +1100

    reverting this

--- bin/staging_smoke_test.sh
+++ bin/staging_smoke_test.sh
@@ -10,4 +10,4 @@ ansible-playbook -i "localhost," -c local provisioning/ci.yml
 npm install
 node_modules/sequelize-cli/bin/sequelize db:migrate # even though we don't use it, the app still needs it to wake up. we can fix this later.
 
-EMAIL_SERVER=/usr/sbin/sendmail INSTANCE_URL=https://project-m-staging.herokuapp.com node --harmony node_modules/jasmine/bin/jasmine.js spec/integration/newMemberSignsUpAndMakesAPaymentSpec.js
+INSTANCE_URL=https://project-m-staging.herokuapp.com node --harmony node_modules/jasmine/bin/jasmine.js spec/integration/newMemberSignsUpAndMakesAPaymentSpec.js
