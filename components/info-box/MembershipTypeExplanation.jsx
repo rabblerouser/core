0cfcb64314ec1fd75dd935d576ef3ce918e57884
Florencia <fleur94@users.noreply.github.com>
2016-02-03 17:15:58 +1100

    partial changes to membership modal

--- components/info-box/MembershipTypeExplanation.jsx
+++ components/info-box/MembershipTypeExplanation.jsx
@@ -34,35 +34,35 @@ export default class MembershipTypeExplanation extends Component {
     return (
       <div>
         <a onClick={this.openModal}><span className="circled">?</span>  What are the different membership types?</a>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal}
-          style={customStyles} >
-          <div className='modal-header'>
-              <img src ='/images/close_button.svg' onClick={this.closeModal} className='close_button'/>
-              <div className='box-header'>
-                  <img src ='/images/pirate_party_logo.svg' className='logo'/>
-                  <h2>Pirate Party</h2>
-                  <h3>Membership Types</h3>
-              </div>
-          </div>
-          <div className="box-column">
-              <h3>Full Membership</h3>
-              <h4>Australian citizens who do not belong to any other political party.</h4>
-          </div>
-          <div className="box-column">
-              <h3>Supporter Membership</h3>
-              <h4>Members who belong to another Political Party, but wish to support the Pirate Party.</h4>
-          </div>
-          <div className="box-column">
-              <h3>Permanent Resident Membership</h3>
-              <h4>Members who have a permanent resident visa and do not belong to any other political party.</h4>
-          </div>
-          <div className="box-column">
-              <h3>International Membership</h3>
-              <h4>International citizens or members with a temporary visa.</h4>
-          </div>
-        </Modal>
+            <Modal
+              isOpen={this.state.modalIsOpen}
+              onRequestClose={this.closeModal}
+              style={customStyles} >
+                  <div className='modal-header'>
+                      <img src ='/images/close_button.svg' onClick={this.closeModal} className='close_button'/>
+                      <div className='box-header'>
+                          <img src ='/images/pirate_party_logo.svg' className='logo'/>
+                          <h2>Membership Types</h2>
+                      </div>
+                  </div>
+
+                  <div className="box-column">
+                      <h3>Full Membership</h3>
+                      <h4>Australian citizens who do not belong to any other political party.</h4>
+                  </div>
+                  <div className="box-column">
+                      <h3>Supporter Membership</h3>
+                      <h4>Members who belong to another Political Party, but wish to support the Pirate Party.</h4>
+                  </div>
+                  <div className="box-column">
+                      <h3>Permanent Resident Membership</h3>
+                      <h4>Members who have a permanent resident visa and do not belong to any other political party.</h4>
+                  </div>
+                  <div className="box-column">
+                      <h3>International Membership</h3>
+                      <h4>International citizens or members with a temporary visa.</h4>
+                  </div>
+            </Modal>
       </div>
     );
   }
