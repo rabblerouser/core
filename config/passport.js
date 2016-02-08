b851f5899d2d5f385dc762681a08079946c9bc15
Claire Dodd <cdodd@thoughtworks.com>
2016-02-09 10:49:24 +1100

    Went back to using nodeify with a catch logger

--- config/passport.js
+++ config/passport.js
@@ -12,11 +12,10 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    var logger = require('../lib/logger');
-    logger.logInfoEvent('ID ABOUT TO BE DESERIALIZED',id);
-    return AdminUser.findById(id).then((err, value) => {
-        logger.logInfoEvent('VALUE OF FIND IN DESERIALIZED',value);
-        logger.logInfoEvent('ERROR OF FIND IN DESERIALIZED',value);
-        done(value, err);
+    return AdminUser.findById(id)
+    .nodeify(done)
+    .catch((err) => {
+        require('../lib/logger').logError(err, 'failed to deserialize');
+        done(null, false);
     });
 });
