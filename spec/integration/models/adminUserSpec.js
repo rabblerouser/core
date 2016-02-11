a9a2d56726ad1e34fbde54180d74106b49e24941
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-11 16:10:50 +1100

    No more nodeify.

--- spec/integration/models/adminUserSpec.js
+++ spec/integration/models/adminUserSpec.js
@@ -1,54 +1,54 @@
 'use strict';
 
-const specHelper = require("../../support/specHelper.js"),
+const specHelper = require('../../support/specHelper.js'),
       sinon = specHelper.sinon,
-      AdminUser = require("../../../models/index").AdminUser;
+      AdminUser = require('../../../models/index').AdminUser;
 
-describe("AdminUser", () => {
-    let email = "iamapirate@thehighseas.com",
-        password = "yaaaar";
+describe('AdminUser', () => {
+    let email = 'iamapirate@thehighseas.com',
+        password = 'yaaaar';
 
-    describe("authenticate", () => {
+    describe('authenticate', () => {
         let cb;
 
         beforeEach(() => {
             cb = sinon.stub();
         });
 
-        it("calls back with the user if the user exists and can be authenticated", (done) => {
+        it('calls back with the user if the user exists and can be authenticated', (done) => {
             return AdminUser.create({ email: email, password: password })
-                .then((user) => {
+                .then(() => {
                     return AdminUser.authenticate(email, password, cb);
                 }).then(() => {
                     expect(cb).toHaveBeenCalledWith(null, jasmine.objectContaining({
                         email: email
                     }));
-                }).nodeify(done);
+                }).then(done, done.fail);
         });
 
-        it("calls back with false if the user exists but cannot be authenticated", (done) => {
+        it('calls back with false if the user exists but cannot be authenticated', (done) => {
             return AdminUser.create({ email: email, password: password })
-                .then((user) => {
-                    return AdminUser.authenticate(email, "wrongpassword", cb);
+                .then(() => {
+                    return AdminUser.authenticate(email, 'wrongpassword', cb);
                 }).then(() => {
                     expect(cb).toHaveBeenCalledWith(null, false);
-                }).nodeify(done);
+                }).then(done, done.fail);
         });
 
-        it("calls back with false if the user does not exist", (done) => {
+        it('calls back with false if the user does not exist', (done) => {
             return AdminUser.authenticate(email, password, cb)
                 .then(() => {
                     expect(cb).toHaveBeenCalledWith(null, false);
-                }).nodeify(done);
+                }).then(done, done.fail);
         });
     });
 
-    describe("instance", () => {
-        it("has an encrypted password", (done) => {
+    describe('instance', () => {
+        it('has an encrypted password', (done) => {
         return AdminUser.create({ email: email, password: password })
             .then((user) => {
                 expect(user.password).not.toBe(password);
-            }).nodeify(done);
+            }).then(done, done.fail);
         });
     });
 });
