4498a653a8cd86628adcd659c1428bc01e44dfd2
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-16 12:18:14 +1100

    Logging the error is enough, no need to show it on a screen. Plus changing the error page, there is no point in showing a message of an unexpected error

--- app.js
+++ app.js
@@ -53,36 +53,18 @@ app.use(sassMiddleware({
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
+app.use((error, req, res, next) => {
+    require('./lib/logger').logError(error.stack, '[general-application-error]');
+    res.status(error.status || 500);
+    res.render('error');
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-    require('./lib/logger').logError(err);
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    next(error);
 });
 
 membershipRenewalJob.start();
