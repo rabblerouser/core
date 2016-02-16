5047e86da05680c063b2d0637c655b348e3a995f
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-16 20:19:13 +1100

    WIP: reorganise (segregating frontend and backend)

--- spec/backend/integration/newMemberSignsUpAndMakesAPaymentSpec.js
+++ spec/backend/integration/newMemberSignsUpAndMakesAPaymentSpec.js
@@ -1,33 +1,32 @@
 'use strict';
 
-var request = require("supertest-as-promised");
+var request = require('supertest-as-promised');
 
-const instance_url = process.env.INSTANCE_URL,
-    specHelper = require("../../support/specHelper");
+const instance_url = process.env.INSTANCE_URL;
 
-describe("User Flow", () => {
+describe('User Flow', () => {
     let app,
         memberSuffix,
         createdInvoiceId,
         member;
 
     let hasNewMemberAndInvoiceId = (res) => {
-        if (!('newMember' in res.body)) throw new Error("missing created member");
-        if (!('invoiceId' in res.body)) throw new Error("missing invoiceId");
+        if (!('newMember' in res.body)) throw new Error('missing created member');
+        if (!('invoiceId' in res.body)) throw new Error('missing invoiceId');
         createdInvoiceId = res.body.invoiceId;
     };
 
     let invoice = {
-        "totalAmount": "88.88",
-        "paymentType": "deposit",
-        "invoiceId": ''
+        'totalAmount': '88.88',
+        'paymentType': 'deposit',
+        'invoiceId': ''
     };
 
     let successfullyCreatingANewMemberShouldRepondWithA200 = () => {
         return request(app)
-            .post("/members")
-            .set("Content-Type", "application/json")
-            .set("Accept", "application/json")
+            .post('/members')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
             .send(member)
             .expect(200)
             .expect(hasNewMemberAndInvoiceId);
@@ -35,9 +34,9 @@ describe("User Flow", () => {
 
     let postMemberWithExistEmailShouldRespondWithA500 = () => {
         return request(app)
-            .post("/members")
-            .set("Content-Type", "application/json")
-            .set("Accept", "application/json")
+            .post('/members')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
             .send(member)
             .expect(500);
     };
@@ -46,37 +45,37 @@ describe("User Flow", () => {
         invoice.invoiceId = createdInvoiceId;
 
         return request(app)
-            .post("/invoices/update")
-            .set("Content-Type", "application/json")
-            .set("Accept", "application/json")
+            .post('/invoices/update')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
             .send(invoice)
             .expect(200);
     };
 
     let makeMemberWithEmail = email => {
         return {
-            "firstName": "Sherlock",
-            "lastName": "Holmes",
-            "email": email,
-            "dateOfBirth": "22/12/1900",
-            "primaryPhoneNumber": "0396291146",
-            "secondaryPhoneNumber": null,
-            "gender": "horse radish",
-            "residentialAddress": {
-                "address": "222b Baker St",
-                "suburb": "London",
-                "country": "England",
-                "state": "VIC",
-                "postcode": "1234"
+            'firstName': 'Sherlock',
+            'lastName': 'Holmes',
+            'email': email,
+            'dateOfBirth': '22/12/1900',
+            'primaryPhoneNumber': '0396291146',
+            'secondaryPhoneNumber': null,
+            'gender': 'horse radish',
+            'residentialAddress': {
+                'address': '222b Baker St',
+                'suburb': 'London',
+                'country': 'England',
+                'state': 'VIC',
+                'postcode': '1234'
             },
-            "postalAddress": {
-                "address": "303 collins st",
-                "suburb": "melbourne",
-                "country": "australia",
-                "state": "VIC",
-                "postcode": "3000"
+            'postalAddress': {
+                'address': '303 collins st',
+                'suburb': 'melbourne',
+                'country': 'australia',
+                'state': 'VIC',
+                'postcode': '3000'
             },
-            "membershipType": "full"
+            'membershipType': 'full'
         };
     };
 
@@ -86,8 +85,8 @@ describe("User Flow", () => {
             memberSuffix = Date.now();
         }
         else {
-            app = require("../../../src/backend/app");
-            memberSuffix = "";
+            app = require('../../../src/backend/app');
+            memberSuffix = '';
         }
 
         let email = `sherlock${memberSuffix}@holmes.co.uk`;
@@ -95,13 +94,13 @@ describe("User Flow", () => {
         member = makeMemberWithEmail(email);
     });
 
-    it ("member sign up with duplicate email should fail", (done) => {
+    it ('member sign up with duplicate email should fail', (done) => {
         successfullyCreatingANewMemberShouldRepondWithA200()
             .then(postMemberWithExistEmailShouldRespondWithA500)
             .then(done, done.fail);
     }, 60000);
 
-    it ("a new member successfully signs up and then makes a payment", (done) => {
+    it ('a new member successfully signs up and then makes a payment', (done) => {
         successfullyCreatingANewMemberShouldRepondWithA200()
             .then(successfullyCreateInvoiceShouldRespondWithA200)
             .then(done, done.fail);
--- spec/unit/lib/memberValidatorSpec.js
+++ spec/unit/lib/memberValidatorSpec.js
@@ -1,69 +1,69 @@
 'use strict';
 
-const moment = require("moment");
+const moment = require('moment');
 
-var _  = require("lodash");
-require("../../support/specHelper");
+var _  = require('lodash');
+require('../../support/specHelper');
 
-var memberValidator = require("../../../src/lib/memberValidator");
+var memberValidator = require('../../../src/lib/memberValidator');
 
-describe("memberValidator", () => {
-    describe("isValid", () => {
+describe('memberValidator', () => {
+    describe('isValid', () => {
         let validMember = {
-                "firstName": "Sherlock",
-                "lastName": "Holmes",
-                "gender": "horse radish",
-                "email": `sherlock@holmes.co.uk`,
-                "dateOfBirth": "22/12/1900",
-                "primaryPhoneNumber": "0396291146",
-                "secondaryPhoneNumber": "0396291146",
-                "residentialAddress": {
-                    "address": "222b Baker St",
-                    "suburb": "London",
-                    "country": "England",
-                    "state": "VIC",
-                    "postcode": ".-0123string"
+                'firstName': 'Sherlock',
+                'lastName': 'Holmes',
+                'gender': 'horse radish',
+                'email': `sherlock@holmes.co.uk`,
+                'dateOfBirth': '22/12/1900',
+                'primaryPhoneNumber': '0396291146',
+                'secondaryPhoneNumber': '0396291146',
+                'residentialAddress': {
+                    'address': '222b Baker St',
+                    'suburb': 'London',
+                    'country': 'England',
+                    'state': 'VIC',
+                    'postcode': '.-0123string'
                 },
-                "postalAddress": {
-                    "address": "303 collins st",
-                    "suburb": "melbourne",
-                    "country": "Australia",
-                    "state": "VIC",
-                    "postcode": "3000"
+                'postalAddress': {
+                    'address': '303 collins st',
+                    'suburb': 'melbourne',
+                    'country': 'Australia',
+                    'state': 'VIC',
+                    'postcode': '3000'
                 },
-                membershipType: "full"
+                membershipType: 'full'
         };
 
-        let optionalFields = ["secondaryPhoneNumber", "gender"];
+        let optionalFields = ['secondaryPhoneNumber', 'gender'];
         let validMemberWithoutOptionalFields =  _.omit(validMember, optionalFields);
 
-        it("should return empty array of errors on valid member", () => {
+        it('should return empty array of errors on valid member', () => {
             expect(memberValidator.isValid(validMember)).toEqual([]);
         });
 
-        it("should return empty array of errors on valid member without optional fields", () => {
+        it('should return empty array of errors on valid member without optional fields', () => {
             expect(memberValidator.isValid(validMemberWithoutOptionalFields)).toEqual([]);
         });
 
-        it("should return array of errors on null member", () => {
+        it('should return array of errors on null member', () => {
             expect(memberValidator.isValid(null).length).not.toBe(0);
         });
     });
 
-    describe("isValidName", () => {
-        it("Should return true given an alpha name", () => {
-            expect(memberValidator.isValidName("aaa")).toBe(true);
+    describe('isValidName', () => {
+        it('Should return true given an alpha name', () => {
+            expect(memberValidator.isValidName('aaa')).toBe(true);
         });
 
-        it("Should return true if name is a alphanumeric", () => {
-            expect(memberValidator.isValidName("Flo the 1st")).toBe(true);
+        it('Should return true if name is a alphanumeric', () => {
+            expect(memberValidator.isValidName('Flo the 1st')).toBe(true);
         });
 
         [
-            "",
+            '',
             null,
-            "a".repeat(256),
-            "Flo the 1st<"
+            'a'.repeat(256),
+            'Flo the 1st<'
         ].forEach((testCase) => {
             it(`Should return false if name is ${testCase}`, () => {
                 expect(memberValidator.isValidName(testCase)).toBe(false);
@@ -71,11 +71,11 @@ describe("memberValidator", () => {
         });
     });
 
-    describe("isValidGender", () => {
+    describe('isValidGender', () => {
         [
-            "aaa",
-            "Flo the 1st",
-            "",
+            'aaa',
+            'Flo the 1st',
+            '',
             null
         ].forEach((testCase) => {
             it(`Should return true if name is ${testCase}`, () => {
@@ -84,8 +84,8 @@ describe("memberValidator", () => {
         });
 
         [
-            "a".repeat(256),
-            "Flo the 1st<"
+            'a'.repeat(256),
+            'Flo the 1st<'
         ].forEach((testCase) => {
             it(`Should return false if name is ${testCase}`, () => {
                 expect(memberValidator.isValidGender(testCase)).toBe(false);
@@ -93,23 +93,23 @@ describe("memberValidator", () => {
         });
     });
 
-    describe("isValidEmail", () => {
-        it("Should return true given a string with an '@' and a '.'", () => {
-            expect(memberValidator.isValidEmail("aaa@attt.com")).toBe(true);
+    describe('isValidEmail', () => {
+        it('Should return true given a string with an \'@\' and a \'.\'', () => {
+            expect(memberValidator.isValidEmail('aaa@attt.com')).toBe(true);
         });
     });
 
-    describe("isValidPrimaryPhoneNumber", () => {
+    describe('isValidPrimaryPhoneNumber', () => {
         [
-            "+61472817381",
-            "0328171381",
-            "0428171331",
-            "04-2817-133-1",
-            "04 2817 1331",
-            "+1555-555-5555",
-            "+1(555)555-5555",
-            "+65 2345 7908",
-            "+18-1111-1111111"
+            '+61472817381',
+            '0328171381',
+            '0428171331',
+            '04-2817-133-1',
+            '04 2817 1331',
+            '+1555-555-5555',
+            '+1(555)555-5555',
+            '+65 2345 7908',
+            '+18-1111-1111111'
         ].forEach((testCase) => {
             it(`Should return true given a string with a mobile phone number ${testCase}`, () => {
                 expect(memberValidator.isValidPhone(testCase)).toBe(true);
@@ -117,9 +117,9 @@ describe("memberValidator", () => {
         });
 
         [
-            "",
+            '',
             null,
-            "words?"
+            'words?'
         ].forEach((testCase) => {
             it(`Should return false if phone is ${testCase}`, () => {
                 expect(memberValidator.isValidPhone(testCase)).toBe(false);
@@ -127,27 +127,27 @@ describe("memberValidator", () => {
         });
     });
 
-    describe("isValidDateOfBirth", () => {
-        it("Should return true given a string with a dateOfBirth", () => {
-            expect(memberValidator.isValidDate("22/12/1900")).toBe(true);
+    describe('isValidDateOfBirth', () => {
+        it('Should return true given a string with a dateOfBirth', () => {
+            expect(memberValidator.isValidDate('22/12/1900')).toBe(true);
         });
 
-        it("Should return true if the user is 16 years old", () => {
+        it('Should return true if the user is 16 years old', () => {
             let date = moment().subtract(16, 'years').format('DD/MM/YYYY');
             expect(memberValidator.isValidDate(date)).toBe(true);
         });
 
-        it("Should return false if the user is under 16 years old", () => {
+        it('Should return false if the user is under 16 years old', () => {
             let date = moment().subtract(16, 'years').add(1, 'days').format('L');
             expect(memberValidator.isValidDate(date)).toBe(false);
         });
 
         let testCases = [
             null,
-            "",
-            "21 Dec 2015",
+            '',
+            '21 Dec 2015',
             moment().add(7, 'days'),
-            "222/12/1900"
+            '222/12/1900'
         ];
 
         testCases.forEach((input) => {
@@ -157,74 +157,74 @@ describe("memberValidator", () => {
         });
     });
 
-    describe("isValidAddress",() => {
+    describe('isValidAddress',() => {
         let validAddress = {};
 
         beforeEach(() => {
             validAddress = {
-                address: "221b Baker St",
-                suburb: "London",
-                country: "Australia",
-                state: "VIC",
-                postcode: "1234"
-            }
+                address: '221b Baker St',
+                suburb: 'London',
+                country: 'Australia',
+                state: 'VIC',
+                postcode: '1234'
+            };
         });
 
         [
             {address: null},
-            {address: ""},
-            {address: "a".repeat(256)},
+            {address: ''},
+            {address: 'a'.repeat(256)},
 
             {suburb: null},
-            {suburb: ""},
-            {suburb: "a".repeat(256)},
+            {suburb: ''},
+            {suburb: 'a'.repeat(256)},
 
             {state: null},
-            {state: ""},
+            {state: ''},
 
             {country: null},
-            {country: ""},
-            {country: "a".repeat(256)},
-            {country: "Select Country"}
+            {country: ''},
+            {country: 'a'.repeat(256)},
+            {country: 'Select Country'}
         ].forEach((failingTestScenario) => {
-            it("Should return false if given " + JSON.stringify(failingTestScenario), () => {
+            it('Should return false if given ' + JSON.stringify(failingTestScenario), () => {
                 expect(memberValidator.isValidAddress(
                     _.merge(validAddress, failingTestScenario)).length).not.toBe(0);
             });
         });
 
-        it("Should return empty array given an address object'", () => {
+        it('Should return empty array given an address object', () => {
             expect(memberValidator.isValidAddress(validAddress).length).toBe(0);
         });
 
-        it("Should return empty array given an address object with postcode as int for Australian address", () => {
+        it('Should return empty array given an address object with postcode as int for Australian address', () => {
             validAddress.postcode = 1234;
             expect(memberValidator.isValidAddress(validAddress).length).toBe(0);
         });
 
-        it("Should return empty array given an address object with postcode as int for international address", () => {
-            validAddress.country = "England";
+        it('Should return empty array given an address object with postcode as int for international address', () => {
+            validAddress.country = 'England';
             validAddress.postcode = 12345678;
             expect(memberValidator.isValidAddress(validAddress).length).toBe(0);
         });
 
-        it("Should return array of errors if address is null", () => {
+        it('Should return array of errors if address is null', () => {
             expect(memberValidator.isValidAddress(null).length).not.toBe(0);
         });
 
-        it("Should return array with 1 error given an address with no state'", () => {
+        it('Should return array with 1 error given an address with no state', () => {
             validAddress.state = null;
             expect(memberValidator.isValidAddress(validAddress).length).toBe(1);
         });
 
         [
             null,
-            "",
-            "-123",
-            "abdt",
-            "1".repeat(5),
+            '',
+            '-123',
+            'abdt',
+            '1'.repeat(5),
         ].forEach((failingTestScenario) => {
-            it("For Australia Address, Should return false if given postcode:" + failingTestScenario, ()=> {
+            it('For Australia Address, Should return false if given postcode:' + failingTestScenario, ()=> {
                 validAddress.postcode = failingTestScenario;
                 expect(memberValidator.isValidAddress(validAddress).length).not.toBe(0);
             });
@@ -232,40 +232,40 @@ describe("memberValidator", () => {
 
         [
             null,
-            "",
-            "1".repeat(17),
+            '',
+            '1'.repeat(17),
         ].forEach((failingTestScenario) => {
-            it("For International Address, Should return false if given postcode:" + failingTestScenario, ()=> {
-                validAddress.country = "England";
+            it('For International Address, Should return false if given postcode:' + failingTestScenario, ()=> {
+                validAddress.country = 'England';
                 validAddress.postcode = failingTestScenario;
                 expect(memberValidator.isValidAddress(validAddress).length).not.toBe(0);
             });
         });
     });
 
-    describe("isValidMembershipType", () => {
-        it("Should return true if type is full", () => {
-            expect(memberValidator.isValidMembershipType("full")).toBe(true);
+    describe('isValidMembershipType', () => {
+        it('Should return true if type is full', () => {
+            expect(memberValidator.isValidMembershipType('full')).toBe(true);
         });
 
-        it("Should return true if type is permanentResident", () => {
-            expect(memberValidator.isValidMembershipType("permanentResident")).toBe(true);
+        it('Should return true if type is permanentResident', () => {
+            expect(memberValidator.isValidMembershipType('permanentResident')).toBe(true);
         });
 
-        it("Should return true if type is full", () => {
-            expect(memberValidator.isValidMembershipType("supporter")).toBe(true);
+        it('Should return true if type is full', () => {
+            expect(memberValidator.isValidMembershipType('supporter')).toBe(true);
         });
 
-        it("Should return true if type is full", () => {
-            expect(memberValidator.isValidMembershipType("internationalSupporter")).toBe(true);
+        it('Should return true if type is full', () => {
+            expect(memberValidator.isValidMembershipType('internationalSupporter')).toBe(true);
         });
 
         [
-            "",
+            '',
             null,
-            "a".repeat(256),
-            "Flo the 1st<",
-            "fulla"
+            'a'.repeat(256),
+            'Flo the 1st<',
+            'fulla'
         ].forEach((testCase) => {
             it(`Should return false if type is ${testCase}`, () => {
                 expect(memberValidator.isValidMembershipType(testCase)).toBe(false);
