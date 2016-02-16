425e0e4afc55d5640a530fa00cc9a48c62b0af34
Robin Doherty <rdoherty@gmail.com>
2016-02-16 18:03:12 +1100

    WIP: reorganise (segregating frontend and backend)

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
-        './components/App.jsx'
+        './src/frontend/components/App.jsx'
     ],
     output: {
         path: path.join(__dirname, 'dist'),
