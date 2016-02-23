bc5d36b7b8f20f653aed44be7ea158066e79cacd
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-23 17:35:56 +1100

    Revert "Fixed path"
    This reverts commit 22b54bbaee26c1f3093337c79383c40a00061868.

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@
 
 const passport = require('passport'),
       LocalStrategy = require('passport-local').Strategy,
-      AdminUser = require('../src/backend/models/index').AdminUser;
+      AdminUser = require('../models/index').AdminUser;
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
