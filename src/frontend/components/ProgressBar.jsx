09c6c40e443771546d5f140b1b14220e3b80a6b0
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-24 16:12:55 +1100

    Readding the progress bar because it is pretty

--- src/frontend/components/ProgressBar.jsx
+++ src/frontend/components/ProgressBar.jsx
@@ -19,6 +19,11 @@ export default class ProgressBar extends Component {
     render() {
        return <div className="header">
                  <img src ='/images/the_lab_logo.svg'/>
+                 <ul className="progress-bar">
+                     <li className={this.getClass(1)} id="progress-details">Details<br/><br/><br/></li>
+                     <li className={this.getClass(2)} id="progress-finished">Finish<br/><br/><br/></li>
+                 </ul>
+
              </div>
     }
 }
