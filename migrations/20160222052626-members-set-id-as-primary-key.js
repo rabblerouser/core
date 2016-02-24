1a4ce84c6b8d53f92d9b2f41bbb460a6ca20fd44
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-24 14:20:57 +1100

    Removed everything related to invoices in migrations

--- migrations/20160222052626-members-set-id-as-primary-key.js
+++ migrations/20160222052626-members-set-id-as-primary-key.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize, done) {
-     queryInterface.dropTable('Invoices')
-    .then(() => {
-      return queryInterface.sequelize.query('ALTER TABLE "Members" DROP CONSTRAINT "Members_pkey"');
-    })
+    queryInterface.sequelize.query('ALTER TABLE "Members" DROP CONSTRAINT "Members_pkey"')
     .then(() => {
        return queryInterface.sequelize.query('ALTER TABLE "Members" ADD CONSTRAINT "Members_pkey" PRIMARY KEY(id)');
     })
@@ -14,42 +11,9 @@ module.exports = {
 
   down: function (queryInterface, Sequelize, done) {
 
-    queryInterface.createTable('Invoices', {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER
-        },
-        memberEmail: {
-          type: Sequelize.STRING,
-          allowNull: false
-        },
-        totalAmount: {
-            type: Sequelize.BIGINT,
-            allowNull: false
-        },
-        paymentDate: {
-            allowNull: false,
-            type: Sequelize.DATE
-        },
-        paymentType: {
-          allowNull: false,
-          type: Sequelize.STRING
-        },
-        reference: {
-          allowNull: false,
-          type: Sequelize.STRING
-        }
-    })
-    .then(() => {
-      return queryInterface.sequelize.query('ALTER TABLE "Members" DROP CONSTRAINT "Members_pkey"');
-    })
+    queryInterface.sequelize.query('ALTER TABLE "Members" DROP CONSTRAINT "Members_pkey"')
     .then(() => {
       return queryInterface.sequelize.query('ALTER TABLE "Members" ADD PRIMARY KEY (email)');
-    })
-    .then(() => {
-      return queryInterface.sequelize.query('ALTER TABLE "Invoices" ADD CONSTRAINT "Invoices_memberEmail_fkey" FOREIGN KEY ("memberEmail") REFERENCES "Members" (email) MATCH FULL');
     })
     .nodeify(done);
   }
