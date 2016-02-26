b2f43276e0a0e6b851d58e63ae356740e4033bbd
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-02-26 11:25:14 +1100

    removing addresses from branches

--- src/backend/models/branch.js
+++ src/backend/models/branch.js
@@ -7,13 +7,6 @@ module.exports = (sequelize, DataTypes) => {
         id: { type: DataTypes.UUID, defaultValue: uuid.v4(), primaryKey: true },
         name: DataTypes.STRING,
         key: { type: DataTypes.UUID, defaultValue: uuid.v4()}
-    }, {
-        classMethods: {
-            associate: (models) => {
-                Branch.belongsTo(models.Address, { as: 'postalAddress', foreignKey: 'postalAddressId' });
-                Branch.belongsTo(models.Address, { as: 'residentialAddress', foreignKey: 'residentialAddressId'});
-            }
-        }
     });
 
     return Branch;
