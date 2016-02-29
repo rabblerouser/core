45456acf9ce1c4d7f69fbe72e935080bd3a5cb94
Robin Doherty <rdoherty@gmail.com>
2016-03-01 09:12:00 +0930

    simple table with hard coded data

--- src/frontend/components/admin/AdminDashboard.jsx
+++ src/frontend/components/admin/AdminDashboard.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import Secretary from './Secretary.jsx';
 
 export default class AdminDashboard extends Component {
     constructor(props) {
@@ -8,14 +7,35 @@ export default class AdminDashboard extends Component {
         this.render = this.render.bind(this);
     }
 
-
     render() {
-            return (
-                <div className="admin-container">
-                    <div className="container">
-                        <Secretary />
-                    </div>
-                </div>);
+
+        let data = [
+            {
+                name: 'React.js'
+            },
+            {
+                name: 'Angular.js'
+            },
+            {
+                name: 'Aurelia'
+            }
+        ];
+
+        let columns = [
+            {
+                property: 'name',
+                header: 'Name'
+            }
+        ];
+
+        let Table = require('reactabular').Table;
+
+        return (
+            <div className="admin-container">
+                <div className="container">
+                    <Table columns={columns} data={data} />
+                </div>
+            </div>);
     }
 }
 
