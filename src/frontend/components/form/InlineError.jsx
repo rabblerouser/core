9bc711178834a62986fbfd6eeb66ef8ca3c86afb
Katherine Alsop <kalsop@Katherines-MacBook-Pro.local>
2016-03-01 14:25:03 +1100

    Fixed styling of school type error. It's now red as it should be

--- src/frontend/components/form/InlineError.jsx
+++ src/frontend/components/form/InlineError.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { ApplictionFormValidationErrors as Errors } from '../../config/strings.js';
 
 export default ({ errorFor }) => (
-  <span className="errors">
+  <span className="invalid">
+  	<span className="errors">
       { Errors[errorFor] ? Errors[errorFor].message : '' }
+  	</span>
   </span>
 )
