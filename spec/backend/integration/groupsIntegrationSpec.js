d35f5f95b37801204a55c79f4cb463e90970be6c
Andrew Jones <andrew@andrewjones.id.au>
2016-03-02 17:08:47 +1100

    add first go at (skipped) adding member to group integration test

--- spec/backend/integration/groupsIntegrationSpec.js
+++ spec/backend/integration/groupsIntegrationSpec.js
@@ -1,8 +1,11 @@
 'use strict';
 
 let request = require('supertest-as-promised');
+let _ = require('lodash');
 const instance_url = process.env.INSTANCE_URL;
-let app;
+let app,
+    agent;
+let integrationTestHelpers = require('./integrationTestHelpers');
 
 let listOfGroups = (res) => {
     if (!('groups' in res.body)) {
@@ -21,10 +24,42 @@ describe('Groups Integration Test', () => {
 
     beforeEach(() => {
         app = instance_url ? instance_url : require('../../../src/backend/app');
+        agent = request.agent(app);
     });
 
     it('should return a list of groups', (done) => {
         getGroups()
         .then(done, done.fail);
     }, 60000);
+
+    xdescribe('adding a member to a group', () => {
+        it('responds with a 200', () => {
+            let group, branch;
+            integrationTestHelpers.createBranch()
+                .tap((branch) => {
+                    branch = branch;
+                })
+                .tap(integrationTestHelpers.createUser)
+                .then(integrationTestHelpers.createFakeMembers(agent, 1))
+                .then((branch) => {
+                    return integrationTestHelpers.createGroup(branch.id)
+                })
+                .tap((group) => {
+                    groupId = group.id;
+                })
+                .then(integrationTestHelpers.authenticate(agent))
+                .then(() => {
+                    return agent.get('/members');
+                })
+                .then((response) => {
+                    let member = _.sample(response.body.members);
+                    let branchId = member.branchId;
+                    return agent.post(`/branches/${branchId}/group/${groupId}/members`)
+                        .set('Content-Type', 'application/json')
+                        .send([member.id])
+                        .expect(200);
+                })
+                .then(done, done.fail);
+        });
+    });
 });
