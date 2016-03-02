f112fce4945592498c210798c9ba0787753b9b87
Dan Mutton <daniel.mutton@gmail.com>
2016-03-03 09:35:19 +1100

    Move seed to beforeEach and beautify

--- spec/backend/integration/branchServiceSequelizeIntegrationSpec.js
+++ spec/backend/integration/branchServiceSequelizeIntegrationSpec.js
@@ -1,84 +1,82 @@
 'use strict';
 
 const specHelper = require('../../support/specHelper'),
-      Branch = specHelper.models.Branch,
-      Group = specHelper.models.Group;
+    Branch = specHelper.models.Branch,
+    Group = specHelper.models.Group;
 
 var uuid = require('node-uuid');
 var branchService = require('../../../src/backend/services/branchService');
 
-function tearDown() {
-    return Group.truncate({cascade: true})
-        .then(Branch.truncate({cascade: true}));
-}
-
 function seed() {
     let branchWithGroups;
-    return Branch.create({id: 'aa3538ae-763d-49d9-ac23-2281ba2145e4', name: 'Branch name no groups'})
-    .then(function() {
-        return Branch.create({id: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174', name: 'Branch name groups'});
-    })
-    .then(function(branch) {
-      branchWithGroups = branch;
-      return Group.create({ id: uuid(), name: 'Waiting List', description: 'This is a description of the waiting list' });
-    })
-    .then(function(group) {
-      return branchWithGroups.addGroup(group);
-    })
-  .then(function() {
-      return Group.create({ id: uuid(), name: 'Waiting List2', description: 'This is a description of the waiting list2' });
-  })
-  .then(function(group) {
-      return branchWithGroups.addGroup(group);
-  });
+    return Branch.create({
+            id: 'aa3538ae-763d-49d9-ac23-2281ba2145e4',
+            name: 'Branch name no groups'
+        })
+        .then(function() {
+            return Branch.create({
+                id: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174',
+                name: 'Branch name groups'
+            });
+        })
+        .then(function(branch) {
+            branchWithGroups = branch;
+            return Group.create({
+                id: uuid(),
+                name: 'Waiting List',
+                description: 'This is a description of the waiting list'
+            });
+        })
+        .then(function(group) {
+            return branchWithGroups.addGroup(group);
+        })
+        .then(function() {
+            return Group.create({
+                id: uuid(),
+                name: 'Waiting List2',
+                description: 'This is a description of the waiting list2'
+            });
+        })
+        .then(function(group) {
+            return branchWithGroups.addGroup(group);
+        });
 }
 
 describe('branchService', () => {
+
+    beforeEach((done) => {
+        seed().nodeify(done);
+
+    });
 
     describe('groupsInBranch', () => {
 
         it('should return the branches groups if it has some', (done) => {
             let branchWithGroupsId = 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174';
-
-            tearDown()
-            .then(seed)
-            .then(() => {
-                branchService.groupsInBranch(branchWithGroupsId)
-                    .then((result) => {
-                        expect(result.length).toEqual(2);
-                    }).then(done, done.fail);
-
-            });
+            branchService.groupsInBranch(branchWithGroupsId)
+                .then((result) => {
+                    expect(result.length).toEqual(2);
+                }).then(done, done.fail);
         });
 
         it('should return no groups if the branch has none', (done) => {
             let branchWithNoGroupsId = 'aa3538ae-763d-49d9-ac23-2281ba2145e4';
-
-            tearDown()
-            .then(seed)
-            .then(() => {
-                branchService.groupsInBranch(branchWithNoGroupsId)
-                    .then((result) => {
-                        expect(result).toEqual([]);
-                    }).then(done, done.fail);
-            });
+            branchService.groupsInBranch(branchWithNoGroupsId)
+                .then((result) => {
+                    expect(result).toEqual([]);
+                }).then(done, done.fail);
         });
 
         describe('sad scenarios', () => {
             it('should handle an invalid branchId', (done) => {
                 let invalidBranchId = 'invalidId';
-
-                tearDown()
-                .then(seed)
-                .then(() => {
-                    branchService.groupsInBranch(invalidBranchId)
-                        .then(() => {
-                            jasmine.fail('This should not have succeded');
-                        })
-                        .catch((error) => {
-                            expect(error.message).toEqual('Error when looking up branch with id: invalidId');
-                        }).then(done, done.fail);
-                });
+                branchService.groupsInBranch(invalidBranchId)
+                    .then(() => {
+                        jasmine.fail('This should not have succeded');
+                    })
+                    .catch((error) => {
+                        expect(error.message).toEqual('Error when looking up branch with id: invalidId');
+                    }).then(done, done.fail);
             });
         });
     });
