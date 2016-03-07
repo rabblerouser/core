f06675a5be0b1ca04e4faa85c65803a72430b455
Katherine Alsop <kalsop@Katherines-MacBook-Pro.local>
2016-03-07 16:43:40 +1100

    Removed extra wording on edit participant modal and styled display of participants' groups in the Admin table

--- src/frontend/components/admin/EditMemberForm.jsx
+++ src/frontend/components/admin/EditMemberForm.jsx
@@ -11,7 +11,7 @@ export default class EditMemberForm extends Component {
     render() {
         return (
             <div>
-                <h2>Update groups for {this.props.participant.firstName} {this.props.participant.lastName}</h2>
+                <h2>{this.props.participant.firstName} {this.props.participant.lastName}</h2>
                 <p>Groups: {this.props.participant.groups}</p>
                 <p>All groups: {JSON.stringify(this.props.groups)}</p>
             </div>
