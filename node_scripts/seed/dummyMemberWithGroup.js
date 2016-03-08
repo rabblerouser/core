223146a846fd9e076b8ff55ea499561b247e5736
Dan Mutton <daniel.mutton@gmail.com>
2016-03-09 09:58:04 +1100

    Slight mod to add group membership

--- node_scripts/seed/dummyMemberWithGroup.js
+++ node_scripts/seed/dummyMemberWithGroup.js
@@ -4,18 +4,26 @@ var Branch = require('../../src/backend/models').Branch;
 var Member = require('../../src/backend/models').Member;
 var Group = require('../../src/backend/models').Group;
 
-var newMember, newGroup;
-
-Branch.create({name: 'Branch name'})
-  .then(function() {
-    return Member.create({firstName: 'Sherlock', lastName: 'Holmes', gender: 'horse radish', email: 'sherlock@holmes.co.uk', dateOfBirth: '01/01/1990', primaryPhoneNumber: '0396291146', secondaryPhoneNumber: '0394291146', residentialAddress: null, postalAddress: null, membershipType: 'full', branchId: '0863a85e-e7d8-42db-8b16-80635b7ccbad', schoolType: 'Primary', contactFirstName: 'Jaime', contactLastName: 'Garzon', additionalInfo: 'Lots of information' });
-  })
+var newMember, newGroup, newBranch;
+Member.truncate({cascade: true})
+    .then(Branch.truncate({cascade: true}))
+    .then(function() {
+        return Branch.truncate({cascade: true});
+    })
+    .then(function() {
+        return Branch.create({id: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174', name: 'Branch name groups'});
+    })
+    .then(function(branch) {
+        newBranch = branch;
+        return Member.create({firstName: 'Sherlock', lastName: 'Holmes', gender: 'horse radish', memberSince: new Date(), email: 'sherlock@holmes.co.uk', dateOfBirth: '01/01/1990', primaryPhoneNumber: '0396291146', secondaryPhoneNumber: '0394291146', residentialAddress: null, postalAddress: null, membershipType: 'full', branchId: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174', schoolType: 'Primary', contactFirstName: 'Jaime', contactLastName: 'Garzon', additionalInfo: 'Lots of information'});
+    })
   .then(function(member) {
     newMember = member;
     return Group.create({ name: 'Waiting List', description: 'This is a description of the waiting list' });
   })
   .then(function(group) {
     newGroup = group;
+    newBranch.addGroup(group);
     return newMember.addGroup(newGroup);
   })
   .then(function() {
@@ -29,7 +37,7 @@ Branch.create({name: 'Branch name'})
     return newMember.getGroups();
   })
   .then(function(groups) {
-    console.log(groups)
+    console.log(groups);
   })
   .then(function() {
     console.log('Database seeded.');
