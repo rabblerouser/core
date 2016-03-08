85912fadb83f097e8a61c1ecfb2d530802402ade
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-03-08 13:50:24 +1100

    making it a little bit easier to understand

--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -3,27 +3,32 @@
 const fs = require('fs'),
       path = require('path'),
       Sequelize = require('sequelize'),
-      basename = path.basename(module.filename),
+      indexDotJs = path.basename(module.filename),
       pg = require('pg');
 
+let db = {};
+
 /* https://github.com/sequelize/sequelize/issues/3781 */
-(function becauseOfSequelizeBug() {
+(function becauseOfSomeSequelizeBug() {
     delete pg.native;
 })();
 
-var db = {};
+let connection = require(__dirname + '/../db/connection.js');
 
-var connection = require(__dirname + '/../db/connection.js');
+function sequelizeModelFile(filename) {
+    return (filename.indexOf('.') !== 0) && (filename !== indexDotJs) && (filename.slice(-3) === '.js');
+}
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = connection['import'](path.join(__dirname, file));
+function lookForModelFilesInFolder(folder) {
+    return fs.readdirSync(folder)
+            .filter(sequelizeModelFile);
+}
+
+lookForModelFilesInFolder(__dirname)
+.forEach(function(file) {
+    let model = connection.import(path.join(__dirname, file));
     db[model.name] = model;
-  });
+});
 
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
