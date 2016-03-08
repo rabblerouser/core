cde52f573a95bedc05440b11b5a64a93ed5699a2
Dan Mutton <daniel.mutton@gmail.com>
2016-03-09 10:05:10 +1100

    Add select all filtering

--- src/frontend/components/admin/GroupsList.jsx
+++ src/frontend/components/admin/GroupsList.jsx
@@ -7,9 +7,13 @@ export default ({ groups , editable, onSave, onSelectGroup}) => {
     let groupEntries = groups.map( group => (<GroupListEntry group={group} onSave={onSave} onSelect={onSelectGroup} />));
     let addModalLauncher = editable ? <li className="new"><AddGroupModalLauncher onSave={onSave} /></li> : null;
 
+    function selectAll() {
+        onSelectGroup('');
+    }
+
     return (
         <ul>
-        	<li className="selected">All participants</li>
+        	<li onClick={selectAll} className="selected">All participants</li>
             { groupEntries }
             { addModalLauncher }
         </ul>
