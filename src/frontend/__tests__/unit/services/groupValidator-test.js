07ed1f748bd56b1c6188676eb8dcf7eec397eca2
Dan Mutton <daniel.mutton@gmail.com>
2016-03-15 21:07:32 +1100

    Create WIP groups validation and wire up

---
+++ src/frontend/__tests__/unit/services/groupValidator-test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import groupValidator from '../../../services/groupValidator';
+
+describe('groupValidator', () => {
+    describe('isValid', () => {
+
+      let validGroup = {
+          'name': 'Valid group',
+          'description': 'some description'
+      };
+
+      it('should return empty array of errors on valid group', () => {
+          expect(groupValidator.isValid(validGroup)).toEqual([]);
+      });
+
+      it('should return array of errors on null group', () => {
+          expect(groupValidator.isValid(null).length).not.toBe(0);
+      });
+
+      it('should return array of errors when missing data', () => {
+        let invalidGroup = {};
+        let expectedErrors = ['name','description'];
+        expect(groupValidator.isValid(invalidGroup)).toEqual(expectedErrors);
+      });
+
+    });
+
+    describe('isValidName', () => {
+        it('Should return true given an alpha name', () => {
+            expect(groupValidator.isValidName('aaa')).toBe(true);
+        });
+
+        it('Should return true if name is a alphanumeric', () => {
+            expect(groupValidator.isValidName('Flo the 1st')).toBe(true);
+        });
+
+        [
+            '',
+            null,
+            Array(257).join('a'),
+            'Flo the 1st<'
+        ].forEach((testCase) => {
+            it(`Should return false if name is ${testCase}`, () => {
+                expect(groupValidator.isValidName(testCase)).toBe(false);
+            });
+        });
+    });
+
+    describe('isValidDescription', () => {
+      it('Should be valid if it is a big string', () => {
+        expect(groupValidator.isValidDescription(Array(2000).join('a'))).toBe(true);
+      });
+
+      [
+          '',
+          null,
+          undefined
+      ].forEach((testCase) => {
+          it(`Should return false if name is ${testCase}`, () => {
+              expect(groupValidator.isValidDescription(testCase)).toBe(false);
+          });
+      });
+
+    });
+});
---
+++ src/frontend/services/groupValidator.js
@@ -0,0 +1,58 @@
+'use strict';
+const validator = require('validator');
+const _ = require('lodash');
+
+const restrictedChars = '[\<\>\"\%\;\(\)\&\+]';
+
+var hasStringValue = (string) => {
+  return !!string;
+};
+
+var isValidString = (inString, pattern) => {
+  pattern = pattern || new RegExp(restrictedChars);
+  return !pattern.test(inString);
+};
+
+var isValidOptionalName = (name) => {
+    return name !== undefined &&
+           isValidString(name) &&
+           name.length < 256;
+};
+
+var isValidName = (name) => {
+  return hasStringValue(name) &&
+         isValidOptionalName(name);
+};
+
+var isValidDescription = (block) => {
+    return hasStringValue(block) &&
+           block.length < 2000;
+};
+
+const groupFieldsChecks =
+{
+    name: isValidName,
+    description: isValidDescription
+};
+
+var isValidDetails = (group) => {
+    return _.reduce(groupFieldsChecks, function(errors, checkFn, groupFieldKey) {
+        if (!group || !checkFn(group[groupFieldKey])){
+            errors.push(groupFieldKey);
+        }
+        return errors;
+    },[]);
+};
+
+var isValid = (group) => {
+    var errors = [
+        isValidDetails(group)
+    ];
+    return _.flatten(errors);
+};
+
+module.exports = {
+    isValid: isValid,
+    isValidName: isValidName,
+    isValidDescription: isValidDescription
+};
