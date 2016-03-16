449c21e9a249e0fb2faa129c526bfe0a3cdb70d9
Dan Mutton <daniel.mutton@gmail.com>
2016-03-16 15:03:13 +1100

    Add participant adapter and tests (WIP - not wired up)

---
+++ src/frontend/__tests__/adapters/participantAdapter-test.js
@@ -0,0 +1,188 @@
+'use strict';
+import participantAdapter from '../../adapters/participantAdapter.js';
+
+describe('participant adapter', () => {
+
+    describe('parseParticipants', () => {
+
+        let validResult = [{
+            id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+            participantName: 'Jo jo',
+            participantLastName: 'The 3rd',
+            contactFirstName: 'Jo',
+            contactLastName: 'The 2nd',
+            contactNumber: '101010010',
+            contactEmail: 'jo@jo.com',
+            participantBirthYear: '1990',
+            schoolType: 'Primary',
+            memberSince: '2016-03-08T22:34:23.721Z',
+            additionalInfo: 'Some additional info',
+            groups: [{id: 1, name: 'Group name'}]
+        }];
+
+        describe('when the payload is valid', () => {
+            let validPayload = {
+                members: [
+                {
+                    id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+                    firstName: 'Jo jo',
+                    lastName: 'The 3rd',
+                    contactFirstName: 'Jo',
+                    contactLastName: 'The 2nd',
+                    primaryPhoneNumber: '101010010',
+                    email: 'jo@jo.com',
+                    dateOfBirth: '1990',
+                    schoolType: 'Primary',
+                    memberSince: '2016-03-08T22:34:23.721Z',
+                    additionalInfo: 'Some additional info',
+                    Groups: [{id: 1, name: 'Group name'}]
+                }]
+            };
+
+            it('should return an array of participants', () => {
+                expect(participantAdapter.parseParticipants(validPayload)).toEqual(validResult);
+            });
+        });
+
+        describe('when the payload is valid, but has additional values', () => {
+            let validPayload = {
+                members: [
+                {
+                    id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+                    firstName: 'Jo jo',
+                    lastName: 'The 3rd',
+                    contactFirstName: 'Jo',
+                    contactLastName: 'The 2nd',
+                    primaryPhoneNumber: '101010010',
+                    email: 'jo@jo.com',
+                    dateOfBirth: '1990',
+                    schoolType: 'Primary',
+                    memberSince: '2016-03-08T22:34:23.721Z',
+                    additionalInfo: 'Some additional info',
+                    Groups: [{id: 1, name: 'Group name'}]
+                }],
+                somethingElse: []
+            };
+
+
+            it('should return an array of participants', () => {
+                expect(participantAdapter.parseParticipants(validPayload)).toEqual(validResult);
+            });
+        });
+
+
+        describe('when the payload is invalid', () => {
+            [
+                {
+                    id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+                    firstName: 'Jo jo',
+                    lastName: 'The 3rd',
+                    contactFirstName: 'Jo',
+                    contactLastName: 'The 2nd',
+                    primaryPhoneNumber: '101010010',
+                    email: 'jo@jo.com',
+                    dateOfBirth: '1990',
+                    schoolType: 'Primary',
+                    memberSince: '2016-03-08T22:34:23.721Z',
+                    additionalInfo: 'Some additional info',
+                    Groups: [{id: 1, name: 'Group name'}]
+                },
+                {participants: {}},
+                {},
+                null
+            ].forEach((testCase) => {
+                it(`Should throw an error on invalid data: ${testCase}`, () => {
+                    expect(() => {participantAdapter.parseParticipants(testCase);}).toThrow();
+                });
+            });
+        });
+    });
+
+    describe('parseParticipant', () => {
+
+        let validResult = {
+            id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+            participantName: 'Jo jo',
+            participantLastName: 'The 3rd',
+            contactFirstName: 'Jo',
+            contactLastName: 'The 2nd',
+            contactNumber: '101010010',
+            contactEmail: 'jo@jo.com',
+            participantBirthYear: '1990',
+            schoolType: 'Primary',
+            memberSince: '2016-03-08T22:34:23.721Z',
+            additionalInfo: 'Some additional info',
+            groups: [{id: 1, name: 'Group name'}]
+        };
+
+        describe('when the payload is valid', () => {
+
+            let validPayload = {
+                id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+                firstName: 'Jo jo',
+                lastName: 'The 3rd',
+                contactFirstName: 'Jo',
+                contactLastName: 'The 2nd',
+                primaryPhoneNumber: '101010010',
+                email: 'jo@jo.com',
+                dateOfBirth: '1990-01-01T00:00:00.000Z',
+                schoolType: 'Primary',
+                memberSince: '2016-03-08T22:34:23.721Z',
+                additionalInfo: 'Some additional info',
+                Groups: [{id: 1, name: 'Group name'}]
+            };
+
+            it('should return a participant object', () => {
+                expect(participantAdapter.parseParticipant(validPayload)).toEqual(validResult);
+            });
+        });
+
+        describe('when the payload is valid, but has additional values', () => {
+            let validPayloadWithExtras = {
+                id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b',
+                firstName: 'Jo jo',
+                lastName: 'The 3rd',
+                contactFirstName: 'Jo',
+                contactLastName: 'The 2nd',
+                primaryPhoneNumber: '101010010',
+                email: 'jo@jo.com',
+                dateOfBirth: '1990-01-01T00:00:00.000Z',
+                schoolType: 'Primary',
+                memberSince: '2016-03-08T22:34:23.721Z',
+                additionalInfo: 'Some additional info',
+                Groups: [{id: 1, name: 'Group name'}],
+                createdAt: '2016-03-13T08:17:37.037Z',
+                updatedAt: '2016-03-13T08:17:37.037Z',
+                deletedAt: null
+            };
+
+            it('should return a participant object', () => {
+                expect(participantAdapter.parseParticipant(validPayloadWithExtras)).toEqual(validResult);
+            });
+
+        });
+
+        describe('when the payload is invalid', () => {
+            [
+                {id: '', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: '', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: '', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: '', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: '', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: '', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: '', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '', additionalInfo: 'Some additional info', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: '', Groups: [{id: 1, name: 'Group name'}]},
+                {id: 'd35048f7-3f06-45e2-8a37-dfb29bbfa81b', firstName: 'Jo jo', lastName: 'The 3rd', contactFirstName: 'Jo', contactLastName: 'The 2nd', primaryPhoneNumber: '101010010', email: 'jo@jo.com', dateOfBirth: '1990-01-01T00:00:00.000Z', schoolType: 'Primary', memberSince: '2016-03-08T22:34:23.721Z', additionalInfo: 'Some additional info', Groups: ''},
+                {},
+                null
+            ].forEach((testCase) => {
+                it(`Should throw an error on invalid data: ${testCase}`, () => {
+                    expect(() => {participantAdapter.parseParticipant(testCase);}).toThrow();
+                });
+            });
+        });
+    });
+});
---
+++ src/frontend/adapters/participantAdapter.js
@@ -0,0 +1,57 @@
+'use strict';
+import moment from 'moment';
+const invalidDataError = 'INVALID DATA';
+
+let parseString = (value) => {
+    if(!value) {
+        throw(invalidDataError);
+    }
+    return value;
+};
+
+let parseYear = (value) => {
+    let year = moment(value).year();
+    if(!value || !year) {
+        throw(invalidDataError);
+    }
+    return year.toString();
+};
+
+let parseGroups = (value) => {
+    if(!value || !Array.isArray(value)) {
+        throw(invalidDataError);
+    }
+    return value;
+};
+
+let parseParticipantDetails = (data) => {
+    if(!data) {
+        throw(invalidDataError);
+    }
+    return {
+        id: parseString(data.id),
+        participantName: parseString(data.firstName),
+        participantLastName: parseString(data.lastName),
+        contactFirstName: parseString(data.contactFirstName),
+        contactLastName: parseString(data.contactLastName),
+        contactNumber: parseString(data.primaryPhoneNumber),
+        contactEmail: parseString(data.email),
+        participantBirthYear: parseYear(data.dateOfBirth),
+        schoolType: parseString(data.schoolType),
+        memberSince: parseString(data.memberSince),
+        additionalInfo: parseString(data.additionalInfo),
+        groups: parseGroups(data.Groups)
+    };
+};
+
+let parseParticipants = (data) => {
+    if(!data || !Array.isArray(data.members)) {
+        throw(invalidDataError);
+    }
+    return data.members.map(parseParticipantDetails);
+};
+
+export default {
+    parseParticipant: parseParticipantDetails,
+    parseParticipants: parseParticipants
+};
