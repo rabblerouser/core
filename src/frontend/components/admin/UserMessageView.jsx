aa816f0f178662209a836b1c963dbbc8cbfbc14e
Dan Mutton <daniel.mutton@gmail.com>
2016-03-16 15:41:28 +1100

    Style user messages

--- src/frontend/components/admin/UserMessageView.jsx
+++ src/frontend/components/admin/UserMessageView.jsx
@@ -2,13 +2,15 @@
 import React from 'react';
 
 export default ({messages, errors}) => {
-    let errorItems = errors.map( (entry) => { return (<li class='error'>{entry}</li>);});
+    let errorItems = errors.map( (entry) => { return (<li className='error'>{entry}</li>);});
     let messageItems = messages.map( (entry) => { return (<li>{entry}</li>);});
 
     return (
-        <ul>
-            {errorItems}
-            {messageItems}
-        </ul>
+        <section className="user_messages">
+            <ul>
+                {errorItems}
+                {messageItems}
+            </ul>
+        </section>
     )
 }
