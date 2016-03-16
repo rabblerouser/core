fef6c3dff98ceebb16e1a5292157229fbae7f248
Robin Doherty <rdoherty@gmail.com>
2016-03-16 11:24:42 +0930

    participant group membership state at EditMemberForm level

--- src/frontend/components/admin/EditMemberForm.jsx
+++ src/frontend/components/admin/EditMemberForm.jsx
@@ -2,25 +2,41 @@
 
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import _ from 'underscore';
 import GroupCheckboxes from './GroupCheckboxes.jsx'
 
 export default class EditMemberForm extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            selectedGroupIds: this.props.participant.Groups.map(group => group.id)
+        };
     }
 
     saveChanges() {
-        this.props.onSave('placeholder');
+        this.props.onSave(this.state.selectedGroupIds);
+    }
+
+    onChange(event) {
+        let newGroups = this.state.selectedGroupIds.slice(0);
+        if(event.target.checked) {
+            newGroups.push(event.target.value);
+        } else {
+            let group = newGroups.find(id => id === event.target.value);
+            newGroups = _.without(newGroups, group);
+        }
+        this.setState({ selectedGroupIds: newGroups });
     }
 
     render() {
         return (
             <section className="form-container">
-                
                 <h2>{this.props.participant.firstName} {this.props.participant.lastName}</h2>
                 <p>Select one or more groups for the participant to join.</p>
-                <GroupCheckboxes allGroups={this.props.participant.allGroups} participantGroups={this.props.participant.Groups} />
-                    <button onClick={this.saveChanges.bind(this)}>Save</button>
+                <GroupCheckboxes onChange={this.onChange.bind(this)}
+                                 allGroups={this.props.participant.allGroups}
+                                 participantGroups={this.props.participant.Groups} />
+                <button onClick={this.saveChanges.bind(this)}>Save</button>
             </section>
         )
     }
--- src/frontend/components/admin/GroupCheckboxes.jsx
+++ src/frontend/components/admin/GroupCheckboxes.jsx
@@ -2,35 +2,20 @@
 
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import _ from 'underscore';
 
 export default class GroupCheckboxes extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedGroupIds: this.props.participantGroups.map(group => group.id)
-        };
     }
 
     isSelected(testGroup) {
         return this.props.participantGroups.find(group => group.id === testGroup.id) !== undefined;
     }
 
-    updateSelection(event) {
-        let newGroups = this.state.selectedGroupIds.slice(0);
-        if(event.target.checked) {
-            newGroups.push(event.target.value);
-        } else {
-            let group = newGroups.find(id => id === event.target.value);
-            newGroups = _.without(newGroups, group);
-        }
-        this.setState({ selectedGroupIds: newGroups});
-    }
-
     render() {
         let checkboxes = this.props.allGroups.map(
             group => {
-                return (<label><input type="checkbox" onChange={this.updateSelection.bind(this)} defaultChecked={this.isSelected(group)} value={group.id} name="{group.id}"/>{group.name}</label>);
+                return (<label key={group.id}><input type="checkbox" onChange={this.props.onChange} defaultChecked={this.isSelected(group)} value={group.id} />{group.name}</label>);
             }
         );
 
