9f3b038085d587841b379aed033ac7404410fa75
Dan Mutton <daniel.mutton@gmail.com>
2016-03-22 17:05:43 +1100

    Also the tests would be nice to include

--- src/frontend/__tests__/unit/services/organiserService-test.js
+++ src/frontend/__tests__/unit/services/organiserService-test.js
@@ -28,6 +28,94 @@ describe('organiserService', () => {
         server.restore();
     });
 
+    describe('delete', () => {
+
+        describe('when the id is valid', () => {
+            beforeEach(() => {
+                server.respondWith('DELETE', '/branches/1234/admins/5678', [200, {
+                        'Content-Type': 'application/json'
+                    },
+                    JSON.stringify('good')
+                ]);
+            });
+
+            it('should send a request to delete the organiser from the lab', (done) => {
+
+                organiserService.delete(organiser, lab)
+                    .then((result) => {
+                        expect(result).toEqual('good');
+                        done();
+                    })
+                    .fail(() => {
+                        done.fail('Expected promise to succeeed');
+                    });
+            });
+
+        });
+
+        describe('when the remote rejects the request', () => {
+            describe('with a 500 server error', () => {
+
+                beforeEach(() => {
+                    server.respondWith('DELETE', '/branches/1234/admins/5678', [500, {
+                        'Content-Type': 'application/json'
+                    }, '']);
+                });
+
+                it('should return a general server error', (done) => {
+                    organiserService.delete(organiser, lab)
+                        .then(() => {
+                            done.fail('Expected promise to be rejected');
+                        })
+                        .fail((error) => {
+                            expect(error.message).toEqual('NOT AVAILABLE');
+                            done();
+                        });
+                });
+            });
+
+            describe('with a 401 unauthorised error', () => {
+
+                beforeEach(() => {
+                    server.respondWith('DELETE', '/branches/1234/admins/5678', [401, {
+                        'Content-Type': 'application/json'
+                    }, '']);
+                });
+
+                it('should return an error that the remote endpoint was not found', (done) => {
+                    organiserService.delete(organiser, lab)
+                        .then(() => {
+                            done.fail('Expected promise to be rejected');
+                        })
+                        .fail((error) => {
+                            expect(error.message).toEqual('NOT FOUND');
+                            done();
+                        });
+                });
+            });
+
+            describe('with a 404 not found error', () => {
+                beforeEach(() => {
+                    server.respondWith('DELETE', '/branches/1234/admins/5678', [404, {
+                        'Content-Type': 'application/json'
+                    }, '']);
+                });
+
+                it('should return an error that the remote endpoint was not found', (done) => {
+                    organiserService.delete(organiser, lab)
+                        .then(() => {
+                            done.fail('Expected promise to be rejected');
+                        })
+                        .fail((error) => {
+                            expect(error.message).toEqual('NOT FOUND');
+                            done();
+                        });
+                });
+            });
+
+        });
+    });
+
     describe('create', () => {
 
         describe('when the organiser provided is valid', () => {
