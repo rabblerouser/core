8aa09cac7132c1a75f9d06574968161688c96b38
Dan Mutton <daniel.mutton@gmail.com>
2016-03-24 09:59:15 +1100

    Should send admin details for delete

--- src/frontend/components/admin/adminsView/DeleteAdminButton.jsx
+++ src/frontend/components/admin/adminsView/DeleteAdminButton.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 export default ({ onDelete, admin }) => {
 
-    var deleteOrganiser = () => {
+    var deleteAdmin = () => {
         if(confirm('Are you sure you want to delete the selected person?')){
             onDelete(admin);
         }
     };
 
-    return (<div className="buttons"><button onClick={onDelete} className="delete" title="Delete admin"><span>Delete admin</span></button></div>)
+    return (<div className="buttons"><button onClick={deleteAdmin} className="delete" title="Delete admin"><span>Delete admin</span></button></div>)
 }
