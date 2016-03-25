4834cc1409b9cef026a51d9e37e596c795a2631e
Dan Mutton <daniel.mutton@gmail.com>
2016-03-25 17:21:03 +1100

    Move function inside class for direct access to props

--- src/frontend/components/admin/adminsView/AdminsList.jsx
+++ src/frontend/components/admin/adminsView/AdminsList.jsx
@@ -9,43 +9,43 @@ const classnames = require('classnames');
 import EditAdminModalLauncher from './EditAdminModalLauncher.jsx';
 import DeleteAdminButton from './DeleteAdminButton.jsx';
 
-function tableColumns(onSave, onDelete) {
-    return [
-        {
-            property: 'name',
-            header: 'Name',
-            headerClass: classnames('name')
-        },
-        {
-            property: 'phoneNumber',
-            header: 'Phone',
-            headerClass: classnames('contact')
-        },
-        {
-            property: 'email',
-            header: 'Email',
-            headerClass: classnames('contact')
-        },
-        {
-            cell: (nothing, admin, rowIndex) => {
-                return {
-                    value: (<div>
-                                <EditAdminModalLauncher type={this.props.type} admin={admin[rowIndex]} onSave={ onSave }/>
-                                <DeleteAdminButton type={this.props.type} admin={admin[rowIndex]} onDelete={ onDelete }/>
-                            </div>)
-                };
-            },
-            headerClass: classnames('edit')
-        }
-    ];
-}
-
 export default class AdminsList extends Component {
+
+    tableColumns() {
+        return [
+            {
+                property: 'name',
+                header: 'Name',
+                headerClass: classnames('name')
+            },
+            {
+                property: 'phoneNumber',
+                header: 'Phone',
+                headerClass: classnames('contact')
+            },
+            {
+                property: 'email',
+                header: 'Email',
+                headerClass: classnames('contact')
+            },
+            {
+                cell: (nothing, admin, rowIndex) => {
+                    return {
+                        value: (<div>
+                                    <EditAdminModalLauncher type={this.props.type} admin={admin[rowIndex]} onSave={ this.props.onSave }/>
+                                    <DeleteAdminButton type={this.props.type} admin={admin[rowIndex]} onDelete={ this.props.onDelete }/>
+                                </div>)
+                    };
+                },
+                headerClass: classnames('edit')
+            }
+        ];
+    }
 
     constructor(props) {
         super(props);
         this.state  = {
-            columns: tableColumns(this.props.onSave, this.props.onDelete),
+            columns: this.tableColumns(),
             columnNames: {
                 onClick: (column) => {
                     sortColumn(
@@ -60,11 +60,10 @@ export default class AdminsList extends Component {
 
     render() {
         let data = sortColumn.sort(this.props.admins, this.state.sortingColumn, sortByOrder);
-
         return (
             <section>
             <Table columns={this.state.columns} data={data} columnNames={this.state.columnNames}/>
-            { this.props.admins.length === 0 ? (<aside className="no-entries">{this.props.type ? 'No ' + this.props.type.toLowerCase()+ 's' : ''}</aside>) : ''}
+                { this.props.admins.length === 0 ? (<aside className="no-entries">{this.props.type ? 'No ' + this.props.type.toLowerCase()+ 's' : ''}</aside>) : ''}
         </section>
         );
     }
