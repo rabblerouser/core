8f20298c3c2165626822c02be0427902602034bb
Dan Mutton <daniel.mutton@gmail.com>
2016-03-26 12:30:55 +1100

    Refactor to use integration test helper in line with other tests

--- spec/backend/integration/branchServiceSequelizeIntegrationSpec.js
+++ spec/backend/integration/branchServiceSequelizeIntegrationSpec.js
@@ -1,92 +1,38 @@
 'use strict';
-
-const specHelper = require('../../support/specHelper'),
-    Branch = specHelper.models.Branch,
-    Group = specHelper.models.Group,
-    AdminUser = specHelper.models.AdminUser;
-
-var uuid = require('node-uuid');
+const integrationTestHelpers = require('./integrationTestHelpers.js');
 var branchService = require('../../../src/backend/services/branchService');
 
-function seed() {
-    let branchWithGroups;
-    return Branch.create({
-            id: 'aa3538ae-763d-49d9-ac23-2281ba2145e4',
-            name: 'Branch name no groups'
-        })
-        .then(function() {
-            return Branch.create({
-                id: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174',
-                name: 'Branch name groups'
-            });
-        })
-        .then(function(branch) {
-            branchWithGroups = branch;
-            return Group.create({
-                id: uuid(),
-                name: 'Waiting List',
-                description: 'This is a description of the waiting list'
-            });
-        })
-        .then(function(group) {
-            return branchWithGroups.addGroup(group);
-        })
-        .then(function() {
-            return Group.create({
-                id: uuid(),
-                name: 'Waiting List2',
-                description: 'This is a description of the waiting list2'
-            });
-        })
-        .then(function(group) {
-            return branchWithGroups.addGroup(group);
-        })
-        .then(function() {
-            return AdminUser.create({
-                id: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174',
-                email: 'some.email@email.com',
-                password: 'some password',
-                phoneNumber: '101010',
-                name: 'Danny Dan Dan',
-                branchId: 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174'
-            });
-        });
-}
-
-describe('branchService', () => {
-
-    beforeEach((done) => {
-        seed().nodeify(done);
-    });
+describe('branchServiceSequelizeIntegration', () => {
 
     describe('groupsInBranch', () => {
 
         it('should return the branches groups if it has some', (done) => {
-            let branchWithGroupsId = 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174';
-            branchService.groupsInBranch(branchWithGroupsId)
-                .then((result) => {
-                    expect(result.length).toEqual(2);
-                }).then(done, done.fail);
+            integrationTestHelpers.createBranch()
+            .tap((branch) => integrationTestHelpers.createGroupInBranch(branch.id))
+            .then((branch) => branchService.groupsInBranch(branch.id))
+            .then((result) => {
+                expect(result.length).toEqual(1);
+            }).then(done, done.fail);
         });
 
         it('should return no groups if the branch has none', (done) => {
-            let branchWithNoGroupsId = 'aa3538ae-763d-49d9-ac23-2281ba2145e4';
-            branchService.groupsInBranch(branchWithNoGroupsId)
-                .then((result) => {
-                    expect(result).toEqual([]);
-                }).then(done, done.fail);
+            integrationTestHelpers.createBranch()
+            .then((branch) => branchService.groupsInBranch(branch.id))
+            .then((result) => {
+                expect(result.length).toEqual(0);
+            }).then(done, done.fail);
         });
 
         describe('sad scenarios', () => {
             it('should handle an invalid branchId', (done) => {
-                let invalidBranchId = 'invalidId';
-                branchService.groupsInBranch(invalidBranchId)
-                    .then(() => {
-                        done.fail('This should not have succeded');
-                    })
-                    .catch((error) => {
-                        expect(error.message).toEqual('Error when looking up groups in branch with id: invalidId');
-                    }).then(done, done.fail);
+                integrationTestHelpers.createBranch()
+                .then(() => branchService.groupsInBranch('invalidId'))
+                .then(() => {
+                    done.fail('This should not have succeded');
+                })
+                .catch((error) => {
+                    expect(error.message).toEqual('Error when looking up groups in branch with id: invalidId');
+                }).then(done, done.fail);
             });
         });
     });
