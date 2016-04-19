ea7df66fccbf8560c97604925dec4177d78778fa
Cam Jackson <camjackson89@gmail.com>
2016-04-19 23:05:22 +1000

    eslintify the group service and group validator

--- src/frontend/services/groupService.js
+++ src/frontend/services/groupService.js
@@ -1,45 +1,47 @@
-'use strict';
 import Q from 'q';
 import $ from 'jquery';
 import groupAdapter from '../adapters/groupAdapter.js';
 
-const handleResponseError = function(error) {
- switch(error.status) {
-    case 401 : case 404 : throw new Error('NOT FOUND');
-    default: throw new Error('NOT AVAILABLE');
+const handleResponseError = error => {
+  switch (error.status) {
+    case 401 :
+    case 404 :
+      throw new Error('NOT FOUND');
+    default:
+      throw new Error('NOT AVAILABLE');
   }
 };
 
-const createGroup = function (group, labId) {
-    return Q($.ajax({
-          type: 'POST',
-          url: `/branches/${labId}/groups`,
-          data: group,
-      }))
-      .then(groupAdapter.parseGroup)
-      .catch(handleResponseError);
-};
+const createGroup = (group, labId) => (
+  Q($.ajax({
+    type: 'POST',
+    url: `/branches/${labId}/groups`,
+    data: group,
+  }))
+    .then(groupAdapter.parseGroup)
+    .catch(handleResponseError)
+);
 
-const updateGroup = function (group, labId) {
-    return Q($.ajax({
-          type: 'PUT',
-          url: `/branches/${labId}/groups/${group.id}`,
-          data: group,
-      }))
-      .then(groupAdapter.parseGroup)
-      .catch(handleResponseError);
-};
+const updateGroup = (group, labId) => (
+  Q($.ajax({
+    type: 'PUT',
+    url: `/branches/${labId}/groups/${group.id}`,
+    data: group,
+  }))
+    .then(groupAdapter.parseGroup)
+    .catch(handleResponseError)
+);
 
-const deleteGroup = (group, labId) => {
-    return Q($.ajax({
-          type: 'DELETE',
-          url: `/branches/${labId}/groups/${group.id}`
-      }))
-      .catch(handleResponseError);
-};
+const deleteGroup = (group, labId) => (
+  Q($.ajax({
+    type: 'DELETE',
+    url: `/branches/${labId}/groups/${group.id}`,
+  }))
+    .catch(handleResponseError)
+);
 
 export default {
-    createGroup: createGroup,
-    updateGroup: updateGroup,
-    deleteGroup: deleteGroup
+  createGroup,
+  updateGroup,
+  deleteGroup,
 };
--- src/frontend/services/groupValidator.js
+++ src/frontend/services/groupValidator.js
@@ -1,29 +1,20 @@
-'use strict';
 import inputValidator from '../../backend/lib/inputValidator';
 import _ from 'lodash';
 
-const groupFieldsChecks =
-{
-    name: inputValidator.isValidName,
-    description: inputValidator.isValidTextBlock
+const groupFieldsChecks = {
+  name: inputValidator.isValidName,
+  description: inputValidator.isValidTextBlock,
 };
 
-var isValidDetails = (group) => {
-    return _.reduce(groupFieldsChecks, function(errors, checkFn, groupFieldKey) {
-        if (!group || !checkFn(group[groupFieldKey])){
-            errors.push(groupFieldKey);
-        }
-        return errors;
-    },[]);
-};
+const isValidDetails = group => (
+  _.reduce(groupFieldsChecks, (errors, checkFn, groupFieldKey) => {
+    if (!group || !checkFn(group[groupFieldKey])) {
+      errors.push(groupFieldKey);
+    }
+    return errors;
+  }, [])
+);
 
-var isValid = (group) => {
-    var errors = [
-        isValidDetails(group)
-    ];
-    return _.flatten(errors);
-};
+const isValid = group => _.flatten([isValidDetails(group)]);
 
-module.exports = {
-    isValid: isValid
-};
+module.exports = { isValid };
