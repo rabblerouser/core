be56743c8ac41f624a6f0fdea39ed5ab2731bb1b
Cam Jackson <camjackson89@gmail.com>
2016-04-19 23:23:05 +1000

    eslintify the member service and member validator

--- src/frontend/services/memberService.js
+++ src/frontend/services/memberService.js
@@ -1,76 +1,73 @@
-'use strict';
+import moment from 'moment';
+import Q from 'q';
+import $ from 'jquery';
 
-const moment = require('moment');
-const Q = require('q');
-const $ = require('jquery');
+const handleResponseError = error => {
+  switch (error.status) {
+    case 401 :
+    case 404 :
+      throw new Error('NOT FOUND');
+    default:
+      throw new Error('NOT AVAILABLE');
+  }
+};
 
-const handleResponseError = function(error) {
+const adaptMember = member => {
+  const adapted = {
+    id: member.id,
+    contactFirstName: member.contactName,
+    contactLastName: member.contactLastName,
+    schoolType: member.schoolType,
+    branchId: member.labId,
+    firstName: member.participantName,
+    lastName: member.participantLastName,
+    email: member.contactEmail,
+    dateOfBirth: moment(member.participantBirthYear).format('DD/MM/YYYY'),
+    primaryPhoneNumber: member.contactNumber,
+    secondaryPhoneNumber: member.secondaryPhoneNumber,
+    gender: member.gender,
+    membershipType: member.membershipType,
+    pastoralNotes: member.pastoralNotes,
+    additionalInfo: member.additionalInfo,
+    groups: member.groups,
+  };
 
- switch(error.status) {
-    case 401 : case 404 : throw new Error('NOT FOUND');
-    default: throw new Error('NOT AVAILABLE');
+  if (member.residentialAddress) {
+    adapted.residentialAddress = {
+      address: member.address,
+      suburb: member.suburb,
+      country: member.country,
+      state: member.state,
+      postcode: member.postcode,
+    };
   }
 
-};
-
-function adaptMember(member) {
-    let adapted = {
-        id: member.id,
-        contactFirstName: member.contactName,
-        contactLastName: member.contactLastName,
-        schoolType: member.schoolType,
-        branchId: member.labId,
-        firstName: member.participantName,
-        lastName: member.participantLastName,
-        email: member.contactEmail,
-        dateOfBirth: moment(member.participantBirthYear).format('DD/MM/YYYY'),
-        primaryPhoneNumber: member.contactNumber,
-        secondaryPhoneNumber: member.secondaryPhoneNumber,
-        gender: member.gender,
-        membershipType: member.membershipType,
-        pastoralNotes: member.pastoralNotes,
-        additionalInfo: member.additionalInfo,
-        groups: member.groups
+  if (member.postalAddress) {
+    adapted.postalAddress = {
+      address: member.address,
+      suburb: member.suburb,
+      country: member.country,
+      state: member.state,
+      postcode: member.postcode,
     };
+  }
 
-    if (member.residentialAddress) {
-        adapted.residentialAddress = {
-            address: member.address,
-            suburb: member.suburb,
-            country: member.country,
-            state: member.state,
-            postcode: member.postcode
-        };
-    }
-
-    if (member.postalAddress) {
-        adapted.postalAddress = {
-            address: member.address,
-            suburb: member.suburb,
-            country: member.country,
-            state: member.state,
-            postcode: member.postcode
-        };
-    }
-
-    return adapted;
-}
-
-const update = function (member, branchId) {
-    return Q($.ajax({
-          type: 'PUT',
-          url: `/branches/${branchId}/members/${member.id}`,
-          data: adaptMember(member)
-      }))
-      .catch(handleResponseError)
-      .then((data) => {
-          if(data.id) {
-              return data;
-          }
-          throw new Error('INVALID MEMBER');
-      });
+  return adapted;
 };
 
-export default {
-    update: update
-};
+const update = (member, branchId) => (
+  Q($.ajax({
+    type: 'PUT',
+    url: `/branches/${branchId}/members/${member.id}`,
+    data: adaptMember(member),
+  }))
+    .catch(handleResponseError)
+    .then(data => {
+      if (data.id) {
+        return data;
+      }
+      throw new Error('INVALID MEMBER');
+    })
+);
+
+export default { update };
--- src/frontend/services/memberValidator.js
+++ src/frontend/services/memberValidator.js
@@ -1,38 +1,29 @@
-'use strict';
 import inputValidator from '../../backend/lib/inputValidator';
 import _ from 'lodash';
 
-const memberFieldsChecks =
-{
-    contactName: inputValidator.isValidName,
-    contactLastName: inputValidator.isValidOptionalName,
-    contactEmail: inputValidator.isValidEmail,
-    contactNumber: inputValidator.isValidPhone,
-    participantName: inputValidator.isValidName,
-    participantLastName: inputValidator.isValidOptionalName,
-    participantBirthYear:  inputValidator.isValidYear,
-    schoolType: inputValidator.isValidName,
-    schoolTypeOtherText: inputValidator.isValidOptionalName,
-    additionalInfo: inputValidator.isValidOptionalTextBlock,
-    pastoralNotes: inputValidator.isValidOptionalTextBlock
+const memberFieldsChecks = {
+  contactName: inputValidator.isValidName,
+  contactLastName: inputValidator.isValidOptionalName,
+  contactEmail: inputValidator.isValidEmail,
+  contactNumber: inputValidator.isValidPhone,
+  participantName: inputValidator.isValidName,
+  participantLastName: inputValidator.isValidOptionalName,
+  participantBirthYear: inputValidator.isValidYear,
+  schoolType: inputValidator.isValidName,
+  schoolTypeOtherText: inputValidator.isValidOptionalName,
+  additionalInfo: inputValidator.isValidOptionalTextBlock,
+  pastoralNotes: inputValidator.isValidOptionalTextBlock,
 };
 
-var isValidDetails = (member) => {
-    return _.reduce(memberFieldsChecks, function(errors, checkFn, memberFieldKey) {
-        if (!member || !checkFn(member[memberFieldKey])){
-            errors.push(memberFieldKey);
-        }
-        return errors;
-    },[]);
-};
+const isValidDetails = member => (
+  _.reduce(memberFieldsChecks, (errors, checkFn, memberFieldKey) => {
+    if (!member || !checkFn(member[memberFieldKey])) {
+      errors.push(memberFieldKey);
+    }
+    return errors;
+  }, [])
+);
 
-var isValid = (member) => {
-    var errors = [
-        isValidDetails(member)
-    ];
-    return _.flatten(errors);
-};
+const isValid = member => _.flatten([isValidDetails(member)]);
 
-export default {
-    isValid: isValid
-};
+export default { isValid };
