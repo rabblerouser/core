51dd0e8545ebc4e10e643bf10407ee4e94dc0d61
Cam Jackson <camjackson89@gmail.com>
2016-04-19 22:17:57 +1000

    eslintify the frontend config strings

--- src/frontend/config/strings.js
+++ src/frontend/config/strings.js
@@ -7,23 +7,27 @@ export const Resources = {
   groupListEndPoint: 'groups',
   networkAdminEndPoint: 'admins',
   theLabContactEmail: 'admin@thelab.org.au',
-  theLabHome: 'http://thelab.org.au/'
+  theLabHome: 'http://thelab.org.au/',
 };
 
 export const ApplicationForm = {
-  instructions: 'Please fill in the following information and we\'ll let you know when space becomes available at your preferred Lab.',
+  instructions: 'Please fill in the following information and we\'ll let you know when space becomes available at ' +
+    'your preferred Lab.',
   labPlaceholder: 'Select Lab',
   byoReminder: 'Please be aware that participants will need to bring their own laptop.',
   ageReminder: 'Please note that our target age is between 10 and 16 years',
   additionalInfoAside: 'Is there anything else you think we should know?',
   submitButton: 'Submit',
   validationErrorTitle: 'Please check the following fields:',
-  remoteSubmitErrorTitle: `Sorry, we could not register you this time. Please try again, or contact us at ${Resources.theLabContactEmail}`,
-  remoteLabListErrorTitle: `Sorry, we could not get the list of available labs at this time. Please try again, or contact us at ${Resources.theLabContactEmail}`
+  remoteSubmitErrorTitle: 'Sorry, we could not register you this time. Please try again, or contact us at ' +
+  `${Resources.theLabContactEmail}`,
+  remoteLabListErrorTitle: 'Sorry, we could not get the list of available labs at this time. Please try again, or ' +
+  `contact us at ${Resources.theLabContactEmail}`,
 };
 
 export const AdminDashboard = {
-    RemoteSaveErrorMessage: `Sorry, there was an issue saving the change. Please try again or contact ${Resources.theLabContactEmail} for support.`
+  RemoteSaveErrorMessage: 'Sorry, there was an issue saving the change. Please try again or contact ' +
+  `${Resources.theLabContactEmail} for support.`,
 };
 
 export const ApplicationFormFieldLabels = {
@@ -44,25 +48,28 @@ export const ApplicationFormFieldLabels = {
   password: 'Password',
   confirmedPassword: 'Confirm password',
   notes: 'Lab notes',
-  contact: 'Contact information'
+  contact: 'Contact information',
 };
 
 export const FormValidationErrors = {
-  contactName: {name: 'Contact first name', message: 'Please enter a contact name. No symbols allowed.'},
-  contactLastName: {name: 'Contact last name', message: 'No symbols allowed.'},
-  contactEmail: {name: 'Email address', message: 'Please enter a valid email address i.e. valid@email.com'},
-  contactNumber: {name: 'Contact number', message: 'Please enter a valid phone number.'},
-  participantName: {name: 'Participant first name', message: 'Please enter the participant\'s name. No symbols allowed.'},
-  participantLastName: {name: 'Participant last name', message: 'No symbols allowed.'},
-  participantBirthYear: {name: 'Participant\'s year of birth', message: 'Please select the year of birth.'},
-  schoolType: {name: 'School type', message: 'Please select a school type. No symbols allowed.'},
-  schoolTypeOtherText: {name: 'School type', message: 'No symbols allowed.'},
-  labSelection: {name: 'Lab to join', message: 'Please select a lab to join.'},
-  additionalInfo: {name: 'Additional info', message: 'Maximum 2000 characters'},
-  pastoralNotes: {name: 'Pastoral notes', message: 'Maximum 2000 characters'},
-  groupName: {name: 'Name', message: 'Please enter a group name. No symbols allowed.'},
-  groupDescription: {name: 'Description', message: 'Please enter a description (maximum 2000 characters).'},
-  name: {name: 'Name', message: 'Please enter a name. No symbols allowed.'},
-  confirmedPassword: {name: 'Confirm password', message: 'Passwords must match'},
-  password: {name: 'Password', message: 'Passwords must be at least 12 characters'}
+  contactName: { name: 'Contact first name', message: 'Please enter a contact name. No symbols allowed.' },
+  contactLastName: { name: 'Contact last name', message: 'No symbols allowed.' },
+  contactEmail: { name: 'Email address', message: 'Please enter a valid email address i.e. valid@email.com' },
+  contactNumber: { name: 'Contact number', message: 'Please enter a valid phone number.' },
+  participantName: {
+    name: 'Participant first name',
+    message: 'Please enter the participant\'s name. No symbols allowed.',
+  },
+  participantLastName: { name: 'Participant last name', message: 'No symbols allowed.' },
+  participantBirthYear: { name: 'Participant\'s year of birth', message: 'Please select the year of birth.' },
+  schoolType: { name: 'School type', message: 'Please select a school type. No symbols allowed.' },
+  schoolTypeOtherText: { name: 'School type', message: 'No symbols allowed.' },
+  labSelection: { name: 'Lab to join', message: 'Please select a lab to join.' },
+  additionalInfo: { name: 'Additional info', message: 'Maximum 2000 characters' },
+  pastoralNotes: { name: 'Pastoral notes', message: 'Maximum 2000 characters' },
+  groupName: { name: 'Name', message: 'Please enter a group name. No symbols allowed.' },
+  groupDescription: { name: 'Description', message: 'Please enter a description (maximum 2000 characters).' },
+  name: { name: 'Name', message: 'Please enter a name. No symbols allowed.' },
+  confirmedPassword: { name: 'Confirm password', message: 'Passwords must match' },
+  password: { name: 'Password', message: 'Passwords must be at least 12 characters' },
 };
