e128d9aa5f4aabd0a30674479ec5f8c71c143188
Cam Jackson <camjackson89@gmail.com>
2016-04-25 21:44:57 +1000

    Fix eslint setup for backend

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,9 +5,9 @@ sudo yum install ansible
 
 ansible-playbook -i "localhost," -c local provisioning/ci.yml
 
-echo '##############################'
-echo '### Running frontend tests ###'
-echo '##############################'
+echo '#####################################'
+echo '### Running frontend lint & tests ###'
+echo '#####################################'
 
 cd frontend
 npm install
@@ -15,13 +15,12 @@ npm run ci
 
 cd ..
 
-echo '#############################'
-echo '### Running backend tests ###'
-echo '#############################'
+echo '####################################'
+echo '### Running backend lint & tests ###'
+echo '####################################'
 
 cd backend
 npm install
-npm run ci
-
+npm run test
 
 #TODO: Acceptance tests? Or are those just part of the backend tests?
