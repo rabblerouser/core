952f8a10c965339d3d65c3d7288acd73f60c600e
Cam Jackson <camjackson89@gmail.com>
2016-04-26 20:51:43 +1000

    eslintify the inputValidator (shared across front and back ends)

--- backend/src/lib/inputValidator.js
+++ backend/src/lib/inputValidator.js
@@ -1,89 +1,67 @@
-'use strict';
+// ////////////////////////////////////////// //
+// ATTENTION!                                 //
+// THIS FILE HAS BEEN COPIED TO THE FRONTEND! //
+// IT SHOULD BE EXTRACTED/SHARED SOMEHOW!     //
+// DO NOT MODIFY IT HERE WITHOUT MODIFYING    //
+// THE FRONTEND COPY AS WELL!!!               //
+// ////////////////////////////////////////// //
+
 const validator = require('validator');
 const moment = require('moment');
 const _ = require('lodash');
 const isEmpty = _.isEmpty;
 
-var containsSpecialCharacters = (theString, restricted) => {
-    return restricted.test(theString);
+const containsSpecialCharacters = (theString, restricted) => restricted.test(theString);
+
+const isValidString = (theString, restricted) => (
+  !!theString && !containsSpecialCharacters(theString, restricted || new RegExp('[<>"%;()&+]'))
+);
+
+const isValidOptionalName = name => isEmpty(name) || (isValidString(name) && name.length < 256);
+
+const isValidName = name => isValidString(name) && name.length < 256;
+
+const isValidText = text => !!text && text.length < 256;
+
+const isValidOptionalTextBlock = block => isEmpty(block) || block.length < 100000;
+
+const isValidTextBlock = block => !!block && block.length < 100000;
+
+const isValidEmail = email => validator.isEmail(email);
+
+const isValidOptionalPhoneNumber = input => isEmpty(input) || /^[-+\s()\d]+$/.test(input);
+
+const isValidPhoneNumber = input => /^[-+\s()\d]+$/.test(input);
+
+const isValidPhone = phone => (!!phone) && isValidPhoneNumber(phone);
+
+const isValidDate = date => {
+  const formattedDate = moment(date, 'DD/MM/YYYY', true);
+  return formattedDate.isValid() && formattedDate.isSameOrBefore(moment());
 };
 
-var isValidString = (theString, restricted) => {
-    restricted = restricted || new RegExp('[\<\>\"\%\;\(\)\&\+]');
-    return !!theString &&
-        !containsSpecialCharacters(theString, restricted);
+const isValidYear = number => {
+  const currentYear = new Date().getFullYear();
+  const year = parseInt(number, 10);
+  return year <= currentYear && year >= 1900;
 };
 
-var isValidOptionalName = (name) => {
-    return isEmpty(name) ||
-           (isValidString(name) &&
-           name.length < 256);
-};
+const isValidPassword = input => (!!input) && /^(.){11,200}$/.test(input);
 
-var isValidName = (name) => {
-    return isValidString(name)  &&
-           name.length < 256;
-};
-
-var isValidText = (text) => {
-  return !!text && text.length < 256;
-};
-
-var isValidOptionalTextBlock = (block) => {
-    return isEmpty(block) ||  block.length < 100000;
-};
-
-var isValidTextBlock = (block) => {
-    return !!block && block.length < 100000;
-};
-
-var isValidEmail = (email) => {
-    return validator.isEmail(email);
-};
-
-var isValidOptionalPhoneNumber = (input) => {
-  return isEmpty(input) || /^[-+\s()\d]+$/.test(input);
-};
-
-var isValidPhoneNumber = (input) => {
-  return /^[-+\s()\d]+$/.test(input);
-};
-
-var isValidPhone = (phone) => {
-    return (!!phone) && isValidPhoneNumber(phone);
-};
-
-var isValidDate = (date) => {
-    let formattedDate = moment(date, 'DD/MM/YYYY', true);
-    return formattedDate.isValid() && formattedDate.isSameOrBefore(moment());
-};
-
-var isValidYear = (number) => {
-    let currentYear = new Date().getFullYear();
-    let year = parseInt(number);
-    return year <= currentYear && year >= 1900;
-};
-
-var isValidPassword = (input) => {
-    return (!!input) && /^(.){11,200}$/.test(input);
-};
-
-function isValidUUID(input) {
-    return validator.isUUID(input, 4);
-}
+const isValidUUID = input => validator.isUUID(input, 4);
 
 module.exports = {
-    isValidString: isValidString,
-    isValidName: isValidName,
-    isValidOptionalName: isValidOptionalName,
-    isValidEmail: isValidEmail,
-    isValidPhone: isValidPhone,
-    isValidOptionalPhone: isValidOptionalPhoneNumber,
-    isValidDate: isValidDate,
-    isValidText: isValidText,
-    isValidOptionalTextBlock: isValidOptionalTextBlock,
-    isValidTextBlock: isValidTextBlock,
-    isValidUUID: isValidUUID,
-    isValidYear: isValidYear,
-    isValidPassword: isValidPassword
+  isValidString,
+  isValidName,
+  isValidOptionalName,
+  isValidEmail,
+  isValidPhone,
+  isValidOptionalPhoneNumber,
+  isValidDate,
+  isValidText,
+  isValidOptionalTextBlock,
+  isValidTextBlock,
+  isValidUUID,
+  isValidYear,
+  isValidPassword,
 };
