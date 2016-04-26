ddd236c5393657ca7f46e513ba7894f2d2ef3fcf
Cam Jackson <camjackson89@gmail.com>
2016-04-26 22:32:19 +1000

    Put the top-level npm install back in for now...

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,6 +5,14 @@ sudo yum install ansible
 
 ansible-playbook -i "localhost," -c local provisioning/ci.yml
 
+
+echo '###################################'
+echo '### Doing top-level npm install ###'
+echo '###################################'
+
+#TODO: We want to delete this step soon...
+npm install
+
 echo '#####################################'
 echo '### Running frontend lint & tests ###'
 echo '#####################################'
