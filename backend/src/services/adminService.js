81f9ac625bdf2fa2f7ed93955cd0d9d86a440725
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-05-04 22:33:23 +0800

    Fixed lint errors for backend adminService.

--- backend/src/services/adminService.js
+++ backend/src/services/adminService.js
@@ -1,135 +1,118 @@
-'use strict';
-const Q = require('q'),
-    models = require('../models'),
-    logger = require('../lib/logger'),
-    adminType = require('../security/adminType'),
-    moment = require('moment'),
-    AdminUser = models.AdminUser,
-    uuid = require('node-uuid');
+const Q = require('q');
+const models = require('../models');
+const logger = require('../lib/logger');
+const adminType = require('../security/adminType');
+const moment = require('moment');
+const AdminUser = models.AdminUser;
+const uuid = require('node-uuid');
 
 function createHash() {
   return uuid.v4();
 }
 
 function handleError(logMessage, userMessage) {
-    return function(error) {
-        logger.error(logMessage, { error: error.toString() });
-        throw new Error(userMessage);
-    };
+  return error => {
+    logger.error(logMessage, { error: error.toString() });
+    throw new Error(userMessage);
+  };
 }
 
 function logEvent(saveResult) {
-    logger.info('[admin-user-sign-up-event]', saveResult.dataValues);
+  logger.info('[admin-user-sign-up-event]', saveResult.dataValues);
 }
 
-let transformAdmin = dbResult => {
-    return {
-        id: dbResult.dataValues.id,
-        name: dbResult.dataValues.name,
-        phoneNumber: dbResult.dataValues.phoneNumber,
-        email: dbResult.dataValues.email
-    };
-};
+const transformAdmin = dbResult => ({
+  id: dbResult.dataValues.id,
+  name: dbResult.dataValues.name,
+  phoneNumber: dbResult.dataValues.phoneNumber,
+  email: dbResult.dataValues.email,
+});
 
 function transformAdmins(adapter) {
-    return function (dbResult) {
-        return dbResult.map(adapter);
-    };
+  return dbResult => dbResult.map(adapter);
 }
 
 function save(adminUser) {
   return AdminUser.create.bind(AdminUser)(adminUser);
 }
 
-let setupNewAdmin = (newAdmin) => {
-    return Object.assign({}, newAdmin, {id: createHash(), memberSince: moment()});
-};
+const setupNewAdmin = newAdmin => Object.assign({}, newAdmin, { id: createHash(), memberSince: moment() });
 
+const create = newAdmin =>
+  Q.all(setupNewAdmin(newAdmin))
+    .then(save)
+    .tap(logEvent)
+    .then(transformAdmin)
+    .catch(handleError('[create-admin-failed]', 'Create admin user failed'));
 
-let create = (newAdmin) => {
-    return Q.all(setupNewAdmin(newAdmin))
-          .then(save)
-          .tap(logEvent)
-          .then(transformAdmin)
-          .catch(handleError('[create-admin-failed]', 'Create admin user failed'));
-};
+const deleteAdmin = id =>
+  AdminUser.destroy({ where: { id } })
+    .then(result => {
+      if (!result) {
+        throw new Error('No records were deleted');
+      }
 
-let deleteAdmin = (id) => {
-    return AdminUser.destroy({where: {id: id}})
-    .then((result) => {
-        if(!result) {
-            throw new Error('No records were deleted');
-        }
-
-        logger.info('[delete-admin]', `admin with id ${id} deleted`);
+      logger.info('[delete-admin]', `admin with id ${id} deleted`);
     })
     .catch(handleError('[delete-admin-failed]', `An error has occurred while deleting the admin with id: ${id}`));
-};
 
-let updateAdmin = (newValues) => {
-
-    return AdminUser.findOne({where: {id: newValues.id}})
-    .then( admin => {
-        return admin.update(newValues);
-    })
+const updateAdmin = newValues =>
+  AdminUser.findOne({ where: { id: newValues.id } })
+    .then(admin => admin.update(newValues))
     .tap(() => logger.info('[update-admin]', `admin with id ${newValues.id} updated`))
     .then(transformAdmin)
     .catch(handleError('[update-admin-failed]', `Error when editing admin with id ${newValues.id}`));
 
+const superAdmins = () => {
+  const query = {
+    attributes: [
+      'id',
+      'name',
+      'email',
+      'phoneNumber',
+    ],
+    where: {
+      type: adminType.super,
+    },
+  };
+
+  return AdminUser.findAll(query)
+    .then(result => {
+      if (!result) {
+        throw new Error('');
+      }
+      return result;
+    })
+    .then(transformAdmins(transformAdmin))
+    .catch(handleError('[find-super-admins]', 'Error when looking up super admins'));
 };
 
-let superAdmins = () => {
+const admins = id => {
+  const query = {
+    attributes: [
+      'id',
+      'name',
+      'email',
+      'phoneNumber',
+    ],
+    where: { branchId: id },
+  };
 
-    let query = {
-        attributes: [
-            'id',
-            'name',
-            'email',
-            'phoneNumber'
-        ],
-        where: {
-            type: adminType.super
-        }
-    };
-
-    return AdminUser.findAll(query)
-        .then(result => {
-            if(!result) {
-                throw('');
-            }
-            return result;
-        })
-        .then(transformAdmins(transformAdmin))
-        .catch(handleError('[find-super-admins]', `Error when looking up super admins`));
-};
-
-let admins = (id) => {
-
-    let query = {
-        attributes: [
-            'id',
-            'name',
-            'email',
-            'phoneNumber'
-        ],
-        where: {branchId: id}
-    };
-
-    return AdminUser.findAll(query)
-        .then(result => {
-            if(!result) {
-                throw('');
-            }
-            return result;
-        })
-        .then(transformAdmins(transformAdmin))
-        .catch(handleError('[find-admins-in-branch-by-id-error]', `Error when looking up admins in branch with id: ${id}`));
+  return AdminUser.findAll(query)
+    .then(result => {
+      if (!result) {
+        throw new Error('');
+      }
+      return result;
+    })
+    .then(transformAdmins(transformAdmin))
+    .catch(handleError('[find-admins-in-branch-by-id-error]', `Error when looking up admins in branch with id: ${id}`));
 };
 
 module.exports = {
-    admins: admins,
-    updateAdmin: updateAdmin,
-    create: create,
-    delete: deleteAdmin,
-    superAdmins: superAdmins
+  admins,
+  updateAdmin,
+  create,
+  delete: deleteAdmin,
+  superAdmins,
 };
