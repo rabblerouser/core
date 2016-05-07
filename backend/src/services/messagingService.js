52e4c734f9133aa9679baa055eabcd56f26bafaf
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-05-07 11:57:41 +1000

    Totally harmless correcting of lint errors for a single file. All tests still passing. App still working.

--- backend/src/services/messagingService.js
+++ backend/src/services/messagingService.js
@@ -1,61 +1,59 @@
 'use strict';
 
-let emailUtil = require('../lib/emailUtil');
-let config = require('config');
-let logger = require('../lib/logger');
-let Q = require('q');
+const emailUtil = require('../lib/emailUtil');
+const config = require('config');
+const logger = require('../lib/logger');
+const Q = require('q');
 
 function logAndRethrow(message) {
-    return function (error) {
-        logger.error(message, {error: error.toString()});
-        throw new Error(error);
-    };
+  return error => {
+    logger.error(message, { error: error.toString() });
+    throw new Error(error);
+  };
 }
 
-let emails = {
+const emails = {
   welcome: {
-    logger: (email) => logger.info('[welcome-email-sent]', { email: email }),
+    logger: email => logger.info('[welcome-email-sent]', { email }),
     subject: 'The Pirate Party - Welcome',
-    text: function() { return `Welcome to the Pirate Party!
+    text() {
+      return `Welcome to the Pirate Party!
 
-        You can now start participating and getting involved towards the development of a more secure and transparent Australia.
+      You can now start participating and getting involved towards the development of a more secure and transparent Australia.
 
-        For a list of upcoming meetings and discussions, head to pirateparty.org.au
+      For a list of upcoming meetings and discussions, head to pirateparty.org.au
 
-        Best,
+      Best,
 
-        The Pirate Party`;
-    }
-  }
+      The Pirate Party`;
+    },
+  },
 };
 
 function sendEmail(member, type) {
-    if (!config.get('email.sendEmails')) {
-        return Q.resolve(member);
-    }
+  if (!config.get('email.sendEmails')) {
+    return Q.resolve(member);
+  }
 
-    let options = {
-        to: member.email,
-        subject: type.subject,
-        body: type.text(member)
-    };
+  const options = {
+    to: member.email,
+    subject: type.subject,
+    body: type.text(member),
+  };
 
-    return emailUtil.sendHtmlEmail(options)
-        .then((result) => {
-            return {
-                options: options,
-                message: result
-            };
-        })
-        .tap(type.logger)
-        .catch(logAndRethrow('[email-failed]'));
+  return emailUtil.sendHtmlEmail(options)
+    .then(result => ({
+      options,
+      result,
+    }))
+    .tap(type.logger)
+    .catch(logAndRethrow('[email-failed]'));
 }
 
 function sendWelcomeEmail(member) {
-    return sendEmail(member, emails.welcome);
+  return sendEmail(member, emails.welcome);
 }
 
-
 module.exports = {
-  sendWelcomeEmail: sendWelcomeEmail
+  sendWelcomeEmail,
 };
