5eb3b9ebafa3c0a300c5229528b9bcfa321bf406
Cam Jackson <camjackson89@gmail.com>
2016-05-22 20:58:22 +1000

    Remove The Lab from html titles

--- bin/staging_smoke_test.sh
+++ bin/staging_smoke_test.sh
@@ -10,4 +10,4 @@ ansible-playbook -i "localhost," -c local provisioning/ci.yml
 npm install
 cd backend
 
-INSTANCE_URL=https://lab-assistant-staging.herokuapp.com ./backend/node_modules/.bin/jasmine backend/spec/backend/integration/newMemberSignsUpSpec.js
+INSTANCE_URL=https://rabblerouser-core.herokuapp.com ./backend/node_modules/.bin/jasmine backend/spec/backend/integration/newMemberSignsUpSpec.js
