1e6d267583ea6dd8d2c989c37b753e45af3074ac
Dan Mutton <daniel.mutton@gmail.com>
2016-05-31 22:20:28 +1000

    Remove groups in teardown

--- backend/node_scripts/e2e-seed/teardown.js
+++ backend/node_scripts/e2e-seed/teardown.js
@@ -1,6 +1,6 @@
 const models = require('../../src/models');
-Promise.all([models.Branch, models.AdminUser].map(model => model.truncate({ cascade: true })))
-.then(() => process.stdout.write('Database cleared of branches, admins, organisers.\n'))
+Promise.all([models.Branch, models.AdminUser, models.Group].map(model => model.truncate({ cascade: true })))
+.then(() => process.stdout.write('Database cleared of branches, groups, admins, organisers.\n'))
 .then(() => process.exit(0))
 .catch(err => {
   process.stdout.write(`Error: ${err}`);
