99898b14a50bc202ef541b55c82b3aeebad3464a
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 20:29:51 +0800

    Delinted logger.js as much as I could without going crazy.

--- backend/src/lib/logger.js
+++ backend/src/lib/logger.js
@@ -1,17 +1,17 @@
 'use strict';
-var winston = require('winston'),
-    moment = require('moment');
+
+const winston = require('winston');
+const moment = require('moment');
 
 const winstonLogger = new (winston.Logger)({
-    transports: [new (winston.transports.Console)({
-        timestamp: () => {
-            return `[${moment().toISOString()}]`;
-        },
-        formatter: (options) => {
-            return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
-                (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
-        }
-    })]
+  transports: [new (winston.transports.Console)({
+    timestamp: () => `[${moment().toISOString()}]`,
+    formatter: options =>
+      `${options.timestamp()} ` +
+      `${options.level.toUpperCase()} ` +
+      (undefined !== options.message ? options.message : '') +
+      (options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : ''),
+  })],
 });
 
 module.exports = winstonLogger;
