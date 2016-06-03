356787b81076eebab6ec76e0e9f85ebcd1081e87
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 23:13:54 +0800

    Delinted superAdminOnlyValidator.

--- backend/src/security/superAdminOnlyValidator.js
+++ backend/src/security/superAdminOnlyValidator.js
@@ -4,17 +4,14 @@ const logger = require('../lib/logger');
 const adminType = require('./adminType');
 
 function isSuperAdmin(req) {
-    if (req.user && req.user.type === adminType.super) {
-        return true;
-    }
-    return false;
+  return !!(req.user && req.user.type === adminType.super);
 }
 
-module.exports = function(req, res, next) {
-    if (isSuperAdmin(req)) {
-        next();
-    } else {
-        logger.info('[access-denied]', `User ${req.user ? req.user.email : 'unknown'} tried to access ${req.method} ${req.path}`);
-        res.sendStatus(401);
-    }
+module.exports = (req, res, next) => {
+  if (isSuperAdmin(req)) {
+    next();
+  } else {
+    logger.info('[access-denied]', `User ${req.user ? req.user.email : 'unknown'} tried to access ${req.method} ${req.path}`);
+    res.sendStatus(401);
+  }
 };
