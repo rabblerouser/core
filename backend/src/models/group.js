0e5a674d257f93ef6062f1a0fe385e0b6a7b7773
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 21:15:16 +0800

    Delinted group model.

--- backend/src/models/group.js
+++ backend/src/models/group.js
@@ -3,19 +3,19 @@
 const uuid = require('node-uuid');
 
 module.exports = (sequelize, DataTypes) => {
-    var Group = sequelize.define('Group', {
-        id: { type: DataTypes.UUID, defaultValue: uuid.v4(), primaryKey: true },
-        name: DataTypes.STRING,
-        description: DataTypes.TEXT
-    }, {
-        paranoid: true,
-        classMethods: {
-            associate: (models) => {
-                Group.belongsToMany(models.Member, { through: 'GroupMembers', foreignKey: 'groupId' });
-                Group.belongsToMany(models.Branch, { through: 'BranchGroups', foreignKey: 'groupId' });
-            }
-        }
-    });
+  const Group = sequelize.define('Group', {
+    id: { type: DataTypes.UUID, defaultValue: uuid.v4(), primaryKey: true },
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+  }, {
+    paranoid: true,
+    classMethods: {
+      associate: models => {
+        Group.belongsToMany(models.Member, { through: 'GroupMembers', foreignKey: 'groupId' });
+        Group.belongsToMany(models.Branch, { through: 'BranchGroups', foreignKey: 'groupId' });
+      },
+    },
+  });
 
-    return Group;
+  return Group;
 };
