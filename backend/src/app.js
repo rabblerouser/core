afd81b83e188e13dccfd49464e2db7ef5d439c60
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-04 00:11:06 +0800

    Delinting app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,26 @@
 'use strict';
 
 require('../config/passport');
+const errorLogger = require('./lib/logger');
 
-const express = require('express'),
-      path = require('path'),
-      favicon = require('serve-favicon'),
-      logger = require('morgan'),
-      bodyParser = require('body-parser'),
-      expressSanitized = require('express-sanitized'),
-      helmet = require('helmet'),
-      routes = require('./routes/index'),
-      sassMiddleware = require('node-sass-middleware'),
-      session = require('express-session'),
-      passport = require('passport'),
-      neat = require('node-neat'),
-      app = express(),
-      config = require('config'),
-      SequelizeSessionStore = require('connect-session-sequelize')(session.Store),
-      db = require('./db/connection'),
-      compress = require('compression'),
-      sessionStore = new SequelizeSessionStore({db: db});
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const expressSanitized = require('express-sanitized');
+const helmet = require('helmet');
+const routes = require('./routes/index');
+const sassMiddleware = require('node-sass-middleware');
+const session = require('express-session');
+const passport = require('passport');
+const neat = require('node-neat');
+const app = express();
+const config = require('config');
+const SequelizeSessionStore = require('connect-session-sequelize')(session.Store);
+const db = require('./db/connection');
+const compress = require('compression');
+const sessionStore = new SequelizeSessionStore({ db });
 
 sessionStore.sync();
 app.use(compress());
@@ -33,37 +34,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitized());
 app.use(session({
-    secret: config.session.secret,
-    store: sessionStore,
-    proxy: config.session.proxy,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { domain: config.session.domain, secure: config.session.secureCookie }
+  secret: config.session.secret,
+  store: sessionStore,
+  proxy: config.session.proxy,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { domain: config.session.domain, secure: config.session.secureCookie },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(sassMiddleware({
-    src: path.join(__dirname, '../public'),
-    dest: path.join(__dirname, '../public'),
-    debug: true,
-    outputStyle: 'compressed',
-    includePaths: neat.includePaths
+  src: path.join(__dirname, '../public'),
+  dest: path.join(__dirname, '../public'),
+  debug: true,
+  outputStyle: 'compressed',
+  includePaths: neat.includePaths,
 }), express.static(path.join(__dirname, '../public')));
 
 app.use('/', routes);
 
 app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((error, req, res, next) => {
-    require('./lib/logger').error('[general-application-error]', {error: error.stack});
-    res.status(error.status || 500);
-    res.render('error');
-
-    next(error);
+  errorLogger.error('[general-application-error]', { error: error.stack });
+  res.status(error.status || 500);
+  res.render('error');
+  next(error);
 });
 
 module.exports = app;
