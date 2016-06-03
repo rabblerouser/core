f3a0073c3b0e8d670a02e06d29b4ee90f7801ff5
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 21:12:53 +0800

    Delinted branch model.

--- backend/src/models/branch.js
+++ backend/src/models/branch.js
@@ -3,20 +3,22 @@
 const uuid = require('node-uuid');
 
 module.exports = (sequelize, DataTypes) => {
-    var Branch = sequelize.define('Branch', {
-        id: { type: DataTypes.UUID, defaultValue: uuid.v4(), primaryKey: true },
-        name: DataTypes.STRING,
-        notes: DataTypes.STRING(2000),
-        contact: DataTypes.STRING,
-        key: { type: DataTypes.UUID, defaultValue: uuid.v4()}
-    }, {
-        paranoid: true,
-        classMethods: {
-            associate: (models) => {
-                Branch.belongsToMany(models.Group, { through: 'BranchGroups', foreignKey: 'branchId' });
-            }
-        }
-    });
+  const Branch = sequelize.define('Branch', {
+    id: { type: DataTypes.UUID, defaultValue: uuid.v4(), primaryKey: true },
+    name: DataTypes.STRING,
+    notes: new DataTypes.STRING(2000),
+    contact: DataTypes.STRING,
+    key: { type: DataTypes.UUID, defaultValue: uuid.v4() },
+  }, {
+    paranoid: true,
+    classMethods: {
+      associate: models =>
+        Branch.belongsToMany(
+          models.Group,
+          { through: 'BranchGroups', foreignKey: 'branchId' }
+        ),
+    },
+  });
 
-    return Branch;
+  return Branch;
 };
