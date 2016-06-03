8381923bb46c0f9f8f1a51233ec55bb37e04e02d
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 21:14:15 +0800

    Delinted branchGroup model.

--- backend/src/models/branchGroup.js
+++ backend/src/models/branchGroup.js
@@ -1,12 +1,10 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-    var BranchGroup = sequelize.define('BranchGroup', {
-        groupId: { type: DataTypes.UUID, allowNull: false },
-        branchId: { type: DataTypes.UUID, allowNull: false },
-    }, {
-        paranoid: true
-    });
+  const BranchGroup = sequelize.define('BranchGroup', {
+    groupId: { type: DataTypes.UUID, allowNull: false },
+    branchId: { type: DataTypes.UUID, allowNull: false },
+  }, { paranoid: true });
 
-    return BranchGroup;
+  return BranchGroup;
 };
