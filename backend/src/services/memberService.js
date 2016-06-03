9876b99ae63151da22550ae6aae904b3c986dae9
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 23:49:54 +0800

    Delinting memberService.

--- backend/src/services/memberService.js
+++ backend/src/services/memberService.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const Q = require('q'),
-    models = require('../models'),
-    logger = require('../lib/logger'),
-    moment = require('moment'),
-    Address = models.Address,
-    Group = models.Group,
-    Member = models.Member,
-    uuid = require('node-uuid'),
-    branchService = require('./branchService'),
-    pluck = require('lodash').pluck;
-
+const Q = require('q');
+const models = require('../models');
+const logger = require('../lib/logger');
+const moment = require('moment');
+const Address = models.Address;
+const Group = models.Group;
+const Member = models.Member;
+const uuid = require('node-uuid');
+const branchService = require('./branchService');
+const pluck = require('lodash').pluck;
 
 function createHash() {
   return uuid.v4();
@@ -21,173 +20,160 @@ function save(member) {
 }
 
 function handleError(message) {
-    return function(error) {
-        logger.error(message, { error: error.stack });
-        return models.Sequelize.Promise.reject(message);
-    };
+  return error => {
+    logger.error(message, { error: error.stack });
+    return models.Sequelize.Promise.reject(message);
+  };
 }
 
 function parse(input, residentialAddress, postalAddress) {
-    let residentialAddressId = residentialAddress ? residentialAddress[0].dataValues.id : null;
-    let postalAddressId = postalAddress ? postalAddress[0].dataValues.id : null;
+  const residentialAddressId = residentialAddress ? residentialAddress[0].dataValues.id : null;
+  const postalAddressId = postalAddress ? postalAddress[0].dataValues.id : null;
 
 
-    return Object.assign({}, input, {
-        dateOfBirth: input.dateOfBirth ? moment(input.dateOfBirth, 'DD/MM/YYYY').toDate() : null,
-        residentialAddressId: residentialAddressId,
-        postalAddressId: postalAddressId
-    });
+  return Object.assign({}, input, {
+    dateOfBirth: input.dateOfBirth ? moment(input.dateOfBirth, 'DD/MM/YYYY').toDate() : null,
+    residentialAddressId,
+    postalAddressId,
+  });
 }
 
 function setupNewMember(newMember) {
   return function (residentialAddress, postalAddress) {
-    let memberToSave = parse(newMember, residentialAddress, postalAddress);
-
-    return Object.assign({}, memberToSave, {id: createHash(), memberSince: moment()});
+    const memberToSave = parse(newMember, residentialAddress, postalAddress);
+    return Object.assign({}, memberToSave, { id: createHash(), memberSince: moment() });
   };
 }
 
 function logEvent(saveResult) {
-    logger.info('[member-sign-up-event]', saveResult.dataValues);
+  logger.info('[member-sign-up-event]', saveResult.dataValues);
 }
 
 function findOrCreateAddress(address) {
-    return Q(Address.findOrCreate({where: address, defaults: address}));
+  return Q(Address.findOrCreate({ where: address, defaults: address }));
 }
 
 function getMemberAddresses(member) {
-    let memberAddresses = [];
+  const memberAddresses = [];
 
-    if (member.residentialAddress) {
-        memberAddresses.push(findOrCreateAddress(member.residentialAddress));
-    }
+  if (member.residentialAddress) {
+    memberAddresses.push(findOrCreateAddress(member.residentialAddress));
+  }
 
-    if (member.postalAddress) {
-        memberAddresses.push(findOrCreateAddress(member.postalAddress));
-    }
+  if (member.postalAddress) {
+    memberAddresses.push(findOrCreateAddress(member.postalAddress));
+  }
 
-    return memberAddresses;
+  return memberAddresses;
 }
 
 function assignToBranch(branchId) {
-    return function (member) {
-        return branchService.findById(branchId)
-        .then((branch) => {
-            return Object.assign({}, member, {branchId: branch.id});
-        });
-    };
+  return member =>
+    branchService
+      .findById(branchId)
+      .then(branch => Object.assign({}, member, { branchId: branch.id }));
 }
 
-let createMember = (newMember) => {
-    return Q.all(getMemberAddresses(newMember))
-          .spread(setupNewMember(newMember))
-          .then(assignToBranch(newMember.branchId))
-          .then(save)
-          .tap(logEvent)
-          .then((savedMember) => {
-            return  savedMember.dataValues;
-          })
-          .catch(handleError('Create Member failed'));
-};
+const createMember = newMember =>
+  Q
+    .all(getMemberAddresses(newMember))
+    .spread(setupNewMember(newMember))
+    .then(assignToBranch(newMember.branchId))
+    .then(save)
+    .tap(logEvent)
+    .then(savedMember => savedMember.dataValues)
+    .catch(handleError('Create Member failed'));
 
 function transformMember(dbMember) {
-    return Object.assign({}, dbMember.dataValues, {
-        groups: pluck(dbMember.dataValues.groups, 'dataValues.id')
-    });
+  return Object.assign({}, dbMember.dataValues, {
+    groups: pluck(dbMember.dataValues.groups, 'dataValues.id'),
+  });
 }
 
 function transformMembers(adapter) {
-    return function (memberQueryResult) {
-        return memberQueryResult.map(adapter);
-    };
+  return memberQueryResult => memberQueryResult.map(adapter);
 }
 
 function list(branchId) {
-    if (!branchId) {
-        return Q.resolve([]);
-    }
+  if (!branchId) {
+    return Q.resolve([]);
+  }
 
-    let query = {
-        include: [
-            {
-                model: Group,
-                as: 'groups',
-                through: {
-                    attributes: ['name']
-                }
-            }
-        ],
-        attributes: [
-            'id',
-            'firstName',
-            'lastName',
-            'primaryPhoneNumber',
-            'secondaryPhoneNumber',
-            'contactFirstName',
-            'contactLastName',
-            'dateOfBirth',
-            'email',
-            'schoolType',
-            'additionalInfo',
-            'membershipType',
-            'memberSince',
-            'branchId',
-            'pastoralNotes'
-        ],
-        where: {
-            branchId: branchId
-        }
-    };
+  const query = {
+    include: [{
+      model: Group,
+      as: 'groups',
+      through: { attributes: ['name'] },
+    }],
+    attributes: [
+      'id',
+      'firstName',
+      'lastName',
+      'primaryPhoneNumber',
+      'secondaryPhoneNumber',
+      'contactFirstName',
+      'contactLastName',
+      'dateOfBirth',
+      'email',
+      'schoolType',
+      'additionalInfo',
+      'membershipType',
+      'memberSince',
+      'branchId',
+      'pastoralNotes',
+    ],
+    where: { branchId },
+  };
 
-    return Member.findAll(query)
-        .then(transformMembers(transformMember))
-        .catch(handleError('An error has occurred while fetching members'));
+  return Member
+    .findAll(query)
+    .then(transformMembers(transformMember))
+    .catch(handleError('An error has occurred while fetching members'));
 }
 
 function updateMemberValues(newValues) {
-    return Member.findOne({where: {id: newValues.id}})
-    .then( member => {
-        return member.update(newValues);
-    });
+  return Member
+    .findOne({ where: { id: newValues.id } })
+    .then(member => member.update(newValues));
 }
 
 function updateMemberGroups(groups) {
-    return function (member) {
-        groups = groups || [];
-        return member.setGroups(groups)
-        .then(() => {
-            return member.reload({
-                include: [
-                {
-                    model: Group,
-                    as: 'groups',
-                    through: {
-                        attributes: ['id']
-                    }
-                }
-            ]});
-        });
-    };
+  return member =>
+    member
+      .setGroups(groups || [])
+      .then(() =>
+        member.reload({
+          include: [{
+            model: Group,
+            as: 'groups',
+            through: {
+              attributes: ['id'],
+            },
+          }],
+        })
+      );
 }
 
 function edit(input) {
-    return Member.sequelize.transaction(() => {
-        return Q.all(getMemberAddresses(input))
-        .spread((residentialAddress, postalAddress) => {
-            return parse(input, residentialAddress, postalAddress);
-        })
-        .then(updateMemberValues)
-        .then(updateMemberGroups(input.groups))
-        .then(transformMember);
-    })
+  return Member.sequelize.transaction(() =>
+    Q
+      .all(getMemberAddresses(input))
+      .spread((residentialAddress, postalAddress) =>
+        parse(input, residentialAddress, postalAddress)
+      )
+      .then(updateMemberValues)
+      .then(updateMemberGroups(input.groups))
+      .then(transformMember)
+  )
     .then(updatedMember => {
-        logger.info('[member-details-updated]', {member: updatedMember});
-        return updatedMember;
+      logger.info('[member-details-updated]', { member: updatedMember });
+      return updatedMember;
     })
     .catch(handleError(`Error when editing member with id ${input.id}`));
 }
+
 module.exports = {
-    createMember: createMember,
-    list: list,
-    edit: edit
+  createMember,
+  list,
+  edit,
 };
