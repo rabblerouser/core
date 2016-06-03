60d326852a05d58479cfb8fcdfd832fa905752a3
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 19:40:51 +0800

    Delinted backend ChargeCardError.

--- backend/src/errors/ChargeCardError.js
+++ backend/src/errors/ChargeCardError.js
@@ -1,13 +1,12 @@
 'use strict';
 
 function ChargeCardError(message) {
-    this.message = message;
-    this.name = 'ChargeCardError';
-    Error.captureStackTrace(this, ChargeCardError);
+  this.message = message;
+  this.name = 'ChargeCardError';
+  Error.captureStackTrace(this, ChargeCardError);
 }
 
 ChargeCardError.prototype = Object.create(Error.prototype);
 ChargeCardError.prototype.constructor = ChargeCardError;
 
 module.exports = ChargeCardError;
-
