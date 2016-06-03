4fffda0a03e1319bb5e86a4e977b15143f0aaff1
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 19:39:50 +0800

    Delinted backend db connection.

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,19 +1,19 @@
 'use strict';
 
-let Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/db.json')[env];
+const config = require(`${__dirname}/../../config/db.json`)[env];
 
-let cls = require('continuation-local-storage');
+const cls = require('continuation-local-storage');
 Sequelize.cls = cls.createNamespace('rabblerouser');
 
-var connection;
+let connection;
 
 if (config.use_env_variable) {
-    connection = new Sequelize(process.env[config.use_env_variable]);
+  connection = new Sequelize(process.env[config.use_env_variable]);
 } else {
-    connection = new Sequelize(config.database, config.username, config.password, config);
+  connection = new Sequelize(config.database, config.username, config.password, config);
 }
 
 module.exports = connection;
