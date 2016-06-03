53376d2092b635ceb8586951f47c077dcf31408c
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 20:03:42 +0800

    Delinted backend branchValidator.

--- backend/src/lib/branchValidator.js
+++ backend/src/lib/branchValidator.js
@@ -1,31 +1,27 @@
 'use strict';
 
-let inputValidator = require('./inputValidator');
+const inputValidator = require('./inputValidator');
 const _ = require('lodash');
 
-const branchFieldsChecks =
-{
-    name: inputValidator.isValidText,
-    notes: inputValidator.isValidOptionalTextBlock,
-    contact: inputValidator.isValidOptionalTextBlock
+const branchFieldsChecks = {
+  name: inputValidator.isValidText,
+  notes: inputValidator.isValidOptionalTextBlock,
+  contact: inputValidator.isValidOptionalTextBlock
 };
 
-var isValidDetails = (branch) => {
-    return _.reduce(branchFieldsChecks, function(errors, checkFn, branchFieldKey) {
-        if (!branch || !checkFn(branch[branchFieldKey])){
-            errors.push(branchFieldKey);
-        }
-        return errors;
-    },[]);
-};
+const isValidDetails = branch =>
+  _.reduce(branchFieldsChecks, (errors, checkFn, branchFieldKey) => {
+    if (!branch || !checkFn(branch[branchFieldKey])) {
+      errors.push(branchFieldKey);
+    }
+    return errors;
+  }, []);
 
-var isValid = (branch) => {
-    var errors = [
-        isValidDetails(branch)
-    ];
-    return _.flatten(errors);
+const isValid = branch => {
+  const errors = [isValidDetails(branch)];
+  return _.flatten(errors);
 };
 
 module.exports = {
-    isValid: isValid,
+  isValid,
 };
