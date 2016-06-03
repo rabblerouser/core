9146a5f674ac6602976662a2a69d8b7e1fa599b8
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-03 23:07:51 +0800

    Delinting backend authenticationRequires.

--- backend/src/security/authenticationRequired.js
+++ backend/src/security/authenticationRequired.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const logger = require('../lib/logger');
+
 module.exports = function requireAuth(req, res, next) {
-    if (!req.isAuthenticated()) {
-        require('../lib/logger').info('Attempted unauth access', req.url);
-        req.session.messages = 'You need to login to view this page';
-        res.redirect('/login');
-        return;
-    }
-    next();
+  if (!req.isAuthenticated()) {
+    logger.info('Attempted unauth access', req.url);
+    req.session.messages = 'You need to login to view this page';
+    res.redirect('/login');
+    return;
+  }
+  next();
 };
