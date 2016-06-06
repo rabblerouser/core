45672d820f2113ba86891cc4636f1529bf05e271
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-06 17:32:10 +0800

    Added linting exceptions to make back-end pass linting.

--- backend/src/lib/logger.js
+++ backend/src/lib/logger.js
@@ -6,11 +6,14 @@ const moment = require('moment');
 const winstonLogger = new (winston.Logger)({
   transports: [new (winston.transports.Console)({
     timestamp: () => `[${moment().toISOString()}]`,
+    /* Just trying to make this readable, because I don't really understand it. */
+    /* eslint-disable prefer-template */
     formatter: options =>
       `${options.timestamp()} ` +
       `${options.level.toUpperCase()} ` +
       (undefined !== options.message ? options.message : '') +
       (options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : ''),
+    /* eslint-enable prefer-template */
   })],
 });
 
