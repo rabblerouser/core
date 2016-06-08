87031a07ee3a12648504c0054b3f80037126bb5f
Dan Mutton <daniel.mutton@gmail.com>
2016-06-08 22:06:27 +1000

    Don't need that test here since I moved some logic

--- frontend/src/__tests__/components/common/Table-test.js
+++ frontend/src/__tests__/components/common/Table-test.js
@@ -6,33 +6,21 @@ describe('Table', () => {
   let rendered;
 
   describe('render', () => {
-    describe('when there is data', () => {
-      const data = [{ key: '' }, { key: '' }];
-      const columns = [{ key: '' }, { key: '' }, { key: '' }];
+    const data = [{ key: '' }, { key: '' }];
+    const columns = [{ key: '' }, { key: '' }, { key: '' }];
 
-      beforeEach(() => {
-        rendered = sd.shallowRender(<Table columns={columns} data={data} />);
-      });
-
-      it('creates a row for each datum', () => {
-        const renderedRows = rendered.subTree('tbody').everySubTree('tr');
-        expect(renderedRows.length).toBe(data.length);
-      });
-
-      it('creates a column for each column', () => {
-        const renderedColumns = rendered.everySubTree('ColumnHeader');
-        expect(renderedColumns.length).toBe(columns.length);
-      });
+    beforeEach(() => {
+      rendered = sd.shallowRender(<Table columns={columns} data={data} />);
     });
 
-    describe('when there is no data', () => {
-      beforeEach(() => {
-        rendered = sd.shallowRender(<Table columns={[]} data={[]} />);
-      });
+    it('creates a row for each datum', () => {
+      const renderedRows = rendered.subTree('tbody').everySubTree('tr');
+      expect(renderedRows.length).toBe(data.length);
+    });
 
-      it('has a message that there are no entries', () => {
-        expect(rendered.text()).toContain('No entries found');
-      });
+    it('creates a column for each column', () => {
+      const renderedColumns = rendered.everySubTree('ColumnHeader');
+      expect(renderedColumns.length).toBe(columns.length);
     });
   });
 });
