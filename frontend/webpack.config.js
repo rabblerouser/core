b84388a5bee077616af4e84e3aa6d3f7b0465b11
Cam Jackson <camjackson89@gmail.com>
2016-06-14 22:08:18 +1000

    Make webpack build the sass (take 2)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const neat = require('node-neat').includePaths;
 
 module.exports = {
   entry: {
@@ -21,12 +23,12 @@ module.exports = {
     ],
     loaders: [
       {
-        test: /\.css$/,
-        loader: 'style!css',
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract('style-loader', `css?sourceMap!sass?sourceMap&includePaths[]=${neat}`),
       },
       {
-        test: /\.scss$/,
-        loader: 'style!css!sass',
+        test: /\.svg/,
+        loader: 'svg-url-loader',
       },
       {
         test: /\.jsx?$/,
@@ -39,6 +41,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new ExtractTextPlugin('style.css'),
     new webpack.optimize.UglifyJsPlugin({ minimize: true }),
   ],
   devtool: 'cheap-module-eval-source-map',
@@ -50,16 +53,22 @@ module.exports = {
         secure: false,
         bypass: req => {
           if (req.method === 'GET') {
-            if (['/admin', '/dashboard', '/error', '/index', '/login'].indexOf(req.url) > -1) {
+            if (['/dashboard', '/error', '/index', '/login'].indexOf(req.url) > -1) {
+              // Let WDS serve these routes, returning the corresponding html file contents
               return `${req.url}.html`;
             }
             if (req.url === '/dashboard/admin') {
+              // Same as above but the admin route is tricky
               return '/admin.html';
             }
             if (req.url === '/' || req.url.startsWith('/stylesheets') || req.url.startsWith('/images')) {
+              // Let WDS serve all of these files as static content (where '/' => '/index.html')
+              // Also, it will automatically do this for /javascript, because of the publicPath config
               return req.url;
             }
+            // TODO: Also handle webpack HMR live updates
           }
+          // Let all other requests proxy through to the target mentioned above
           return false;
         },
       },
