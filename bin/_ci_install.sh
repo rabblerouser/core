ba11bda2e700638271fb5c3babd747857ac048d2
Dan Mutton <daniel.mutton@gmail.com>
2016-06-18 16:08:36 +1000

    Add scripts for e2e stage in ci

---
+++ bin/_ci_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+sudo yum install ansible
+
+ansible-playbook -i "localhost," -c local provisioning/ci.yml
+
+npm install
---
+++ bin/_run_e2e_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+echo "Will wait for the server to spin up for testing"
+attempts=0
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+  attempts=$((attempts+1))
+  if [ "$attempts" = '100' ]; then
+    echo "Tried to contact the site, but it's taking too long. Possibly it's broken. Either way it's ignoring me and I don't appreciate this."
+    exit 1
+  fi
+  sleep 5
+done
+npm --prefix e2e test
+exit 0
