0add2a4315196aab8c78e22a5c1043e94d0b0e1d
Dan Mutton <daniel.mutton@gmail.com>
2016-06-25 20:43:47 +1000

    Need to seed in the e2e script now

--- bin/ci_e2e.sh
+++ bin/ci_e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-./bin/_ci_install.sh &&
+./bin/_ci_install.sh && npm run seed && 
 (./bin/_start_server.sh & if ./bin/_run_e2e_tests.sh ; then
   echo 'Successfully completed'
   exit 0
