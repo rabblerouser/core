19e8ff707468113f6b04789b79262215a39f8448
Pam Rucinque <pamela.rucinque@gmail.com>
2016-08-08 00:43:56 +1000

    auth integration migrated to mocha

---
+++ backend/spec/mocha-temp/integration/authenticationIntegrationSpec.js
@@ -0,0 +1,35 @@
+'use strict';
+const instance_url = process.env.INSTANCE_URL;
+let app = instance_url ? instance_url : require('../../../src/app');
+let request = require('supertest-as-promised');
+let integrationTestHelpers = require('./integrationTestHelpers.js');
+
+describe.only('PathAccessValidator Integration tests', () => {
+    let agent;
+
+    beforeEach(() => {
+        agent = request.agent(app);
+
+
+        return integrationTestHelpers.resetDatabase()
+        .then(() => {
+            return integrationTestHelpers.createBranch();
+        })
+        .then(integrationTestHelpers.createBranchAdmin)
+        .then(integrationTestHelpers.authenticateBranchAdmin(agent));
+    });
+
+    it('should respond with 401 when users try to access they are NOT entitled to see', () => {
+        let aDifferentBranchId = 'fd4f7e67-66fe-4f7a-86a6-f031cb3af174';
+
+        return agent.get(`/branches/${aDifferentBranchId}/members`)
+        .expect(401);
+    });
+
+    it('should respond with 401 when users try to access a branch path with no branch id', () => {
+        let aDifferentBranchId;
+
+        return agent.get(`/branches/${aDifferentBranchId}/members`);
+        expect(401);
+    });
+});
