ad2e1f5f56a17d9ef74cc3eb82f233c8262d8ae4
Pam Rucinque <pamela.rucinque@gmail.com>
2016-08-10 12:01:53 +1000

    Revert "attempt to deploy to aws"
    This reverts commit 2d2c1e4da24df238c3bdac2f603549ae1e9a57cd.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,9 +5,7 @@ set -e
 : "${APP_GIT_SHA:?Expecting APP_GIT_SHA to be set}"
 : "${CERT_EMAIL:?Expecting CERT_EMAIL to be set}"
 : "${DB_PASSWORD:?Expecting DB_PASSWORD to be set}"
-: "${STAGING_URL:?Expecting STAGING_URL to be set}"
-: "${SSH_KEY_PATH:?Expecting SSH_KEY_PATH to be set}"
 
 sudo yum install ansible
 
-ansible-playbook -i "${STAGING_URL}," --private-key="${SSH_KEY_PATH}" -u ubuntu provisioning/prod.yml -vvvv
+ansible-playbook -i hosts provisioning/prod.yml
