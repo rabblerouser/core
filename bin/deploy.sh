158b34e320775eeb2fda3e3cc7a1ed934162c021
Courtney Webster <cwebster@thoughtworks.com>
2016-09-02 14:46:51 +1000

    Install ansible with yum instead of apt-get for snap servers

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,9 @@ set -e
 : "${STAGING_URL:?Expecting STAGING_URL to be set}"
 : "${SSH_KEY_PATH:?Expecting SSH_KEY_PATH to be set}"
 
-sudo yum install ansible
+# sudo apt-get install software-properties-common -y
+# sudo apt-add-repository ppa:ansible/ansible -y
+# sudo apt-get update
+# sudo apt-get install ansible -y
 
 ansible-playbook -i "${STAGING_URL}," --private-key="${SSH_KEY_PATH}" -u ubuntu provisioning/prod.yml -vvvv
