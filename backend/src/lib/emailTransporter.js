d22354912d25091ab3e83dc9f390b74ab380548d
Pam Rucinque <pamela.rucinque@gmail.com>
2016-09-15 18:40:10 +1000

    adding support for mailgun

--- backend/src/lib/emailTransporter.js
+++ backend/src/lib/emailTransporter.js
@@ -1,29 +1,47 @@
 const config = require('config');
+const smtpTransport = require('nodemailer-smtp-transport');
 
-module.exports = function() {
+function buildGmailTransport() {
+  if (!(process.env.EMAIL_USERNAME && process.env.EMAIL_PASSWORD)) {
+    throw Error('Environment vars EMAIL_USERNAME and EMAIL_PASSWORD are not defined');
+  }
+
+  return smtpTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+}
+
+function buildMailgunTransport() {
+  if (!(process.env.EMAIL_USERNAME && process.env.EMAIL_PASSWORD)) {
+    throw Error('Environment vars EMAIL_USERNAME and EMAIL_PASSWORD are not defined');
+  }
+
+  return smtpTransport({
+    host: 'smtp.mailgun.org',
+    port: 25,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+}
+
+module.exports = function () {
   const transporterSelected = config.get('email.transporter');
 
   if (!transporterSelected) {
     throw Error('Email transporter not defined');
   }
 
-
-  switch(transporterSelected.toLowerCase()) {
+  switch (transporterSelected.toLowerCase()) {
     case 'gmail':
-        const smtpTransport = require('nodemailer-smtp-transport');
-
-        if (!(process.env.EMAIL_USERNAME && process.env.EMAIL_PASSWORD)) {
-          throw Error('Environment vars EMAIL_USERNAME and EMAIL_PASSWORD are not defined');
-        }
-
-        return smtpTransport({
-            service: 'gmail',
-            auth: {
-              user: process.env.EMAIL_USERNAME,
-              pass: process.env.EMAIL_PASSWORD
-            }
-        });
-    break;
+      return buildGmailTransport();
+    case 'mailgun':
+      return buildMailgunTransport();
 
     default:
       throw Error('Email transporter not supported');
