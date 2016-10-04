eb37d2e100f891905fdf39509406a78030fae766
Dan Mutton <daniel.mutton@gmail.com>
2016-10-04 23:09:40 +1100

    Expending the timeouts across all expectations to get the tests to pass

--- e2e/tests/admin.js
+++ e2e/tests/admin.js
@@ -1,15 +1,15 @@
 const signsIn = client => {
   const login = client.page.login();
   login.navigate().assert.title('Rabble Rouser - Login');
-  login.expect.section('@form').to.be.visible;
+  login.expect.section('@form').to.be.visible.before(1000);
   const form = login.section.form;
-  form.expect.element('@email').to.be.visible;
-  form.expect.element('@password').to.be.visible;
-  form.expect.element('@submitButton').to.be.visible;
+  form.expect.element('@email').to.be.visible.before(1000);
+  form.expect.element('@password').to.be.visible.before(1000);
+  form.expect.element('@submitButton').to.be.visible.before(1000);
   form.setValue('@email', 'admin@rr.com');
   form.setValue('@password', 'apassword');
   form.submit(form);
-  form.expect.element('@submitButton').not.to.be.present;
+  form.expect.element('@submitButton').not.to.be.present.before(1000);
 }
 
 module.exports = {
@@ -18,15 +18,15 @@ module.exports = {
 
     const admin = client.page.admin();
     client.assert.title('Rabble Rouser - Admin');
-    admin.expect.section('@header').to.be.present;
-    admin.expect.section('@branches').to.be.present;
-    admin.expect.section('@organisers').to.be.present;
-    admin.expect.section('@groups').to.be.present;
-    admin.expect.section('@members').to.be.present;
-    admin.expect.section('@admins').to.be.present;
+    admin.expect.section('@header').to.be.present.before(1000);
+    admin.expect.section('@branches').to.be.present.before(1000);
+    admin.expect.section('@organisers').to.be.present.before(1000);
+    admin.expect.section('@groups').to.be.present.before(1000);
+    admin.expect.section('@members').to.be.present.before(1000);
+    admin.expect.section('@admins').to.be.present.before(1000);
 
     const header = admin.section.header;
-    header.expect.element('@logoutButton').to.be.present;
+    header.expect.element('@logoutButton').to.be.present.before(1000);
     header.logout(header);
     client.assert.title('Rabble Rouser - Login');
     client.end();
--- e2e/tests/registration.js
+++ e2e/tests/registration.js
@@ -2,15 +2,15 @@ module.exports = {
   'Registration tests' :  client => {
     const signup = client.page.signup();
     signup.navigate().assert.title('Sign Up');
-    signup.expect.section('@registration').to.be.visible.after(1000);
+    signup.expect.section('@registration').to.be.visible.before(1000);
     const registration = signup.section.registration;
 
-    registration.expect.element('@firstName').to.be.visible.after(1000);
-    registration.expect.element('@lastName').to.be.visible.after(1000);
-    registration.expect.element('@email').to.be.visible.after(1000);
-    registration.expect.element('@primaryPhoneNumber').to.be.visible.after(1000);
-    registration.expect.element('@additionalInfo').to.be.visible.after(1000);
-    registration.expect.element('@submitButton').to.be.visible.after(1000);
+    registration.expect.element('@firstName').to.be.visible.before(1000);
+    registration.expect.element('@lastName').to.be.visible.before(1000);
+    registration.expect.element('@email').to.be.visible.before(1000);
+    registration.expect.element('@primaryPhoneNumber').to.be.visible.before(1000);
+    registration.expect.element('@additionalInfo').to.be.visible.before(1000);
+    registration.expect.element('@submitButton').to.be.visible.before(1000);
 
     registration.setValue('@primaryPhoneNumber', '555100000')
     .setValue('@firstName', 'Margaret')
@@ -18,12 +18,12 @@ module.exports = {
     .setValue('@email', 'mrobins@brooklyn.com')
     .setValue('@additionalInfo', 'just checking in');
     registration.submit(registration);
-    registration.expect.element('@submitButton').not.to.be.present;
-    // signup.expect.section('@registration').not.to.be.present;
-    signup.expect.section('@finished').to.be.present;
+    registration.expect.element('@submitButton').not.to.be.present.before(1000);
+    signup.expect.section('@registration').not.to.be.present.before(1000);
+    signup.expect.section('@finished').to.be.present.before(1000);;
     //
     const finished = signup.section.finished;
-    finished.expect.element('@heading').to.be.visible.after(1000);
+    finished.expect.element('@heading').to.be.visible.before(1000);
     finished.expect.element('@heading').text.to.contain('Thank you, we have received your details.');
 
     client.end();
