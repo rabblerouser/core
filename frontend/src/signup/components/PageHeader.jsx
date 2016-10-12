52aa0b731f33818f3ee3af31df16218cd2c0713e
Cam Jackson <camjackson89@gmail.com>
2016-10-12 21:27:49 +1100

    #73 - Fix header logo, add default images to repo, and clean up legacy image files

--- frontend/src/signup/components/PageHeader.jsx
+++ frontend/src/signup/components/PageHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // img src to be set from CSS.
 const PageHeader = () => (
   <header>
-    <a href={customisation.signupHomepageLink}><img alt="Logo" /></a>
+    <a href={customisation.signupHomepageLink} />
   </header>
 );
 
