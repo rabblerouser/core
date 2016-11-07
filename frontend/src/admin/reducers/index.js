6cf586c8fddab843702a3252df5b189428764688
Dan Mutton <daniel.mutton@gmail.com>
2016-11-07 21:52:03 +1100

    Removed unused reducers (dead code)

--- frontend/src/admin/reducers/index.js
+++ frontend/src/admin/reducers/index.js
@@ -3,14 +3,11 @@ import { reducer as formReducer } from 'redux-form';
 
 import branchReducers from './branchReducers';
 import appFeedbackReducers from './appFeedbackReducers';
-import modalReducers from './modalReducers';
-
 import { reducers as branchView } from '../branchView/';
 
 export default combineReducers({
   branchView,
   branches: branchReducers,
   appFeedback: appFeedbackReducers,
-  modal: modalReducers,
   form: formReducer,
 });
