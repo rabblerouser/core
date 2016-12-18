8651f1bb0bcb8ab6eb6be8b26c53723d4fc969a2
Cam Jackson <camjackson89@gmail.com>
2016-12-18 11:39:34 +1100

    Simplify database config stuff

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,19 +1,18 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../../config/db.json`)[env];
-
+const config = require(`${__dirname}/../../config/db.json`);
 const cls = require('continuation-local-storage');
+
 Sequelize.cls = cls.createNamespace('rabblerouser');
 
 let connection;
 
-if (config.use_env_variable) {
-  connection = new Sequelize(process.env[config.use_env_variable]);
+const dbConfig = config[process.env.NODE_ENV || 'development'];
+if (dbConfig) {
+  connection = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 } else {
-  connection = new Sequelize(config.database, config.username, config.password, config);
+  connection = new Sequelize(process.env.DATABASE_URL);
 }
 
 module.exports = connection;
