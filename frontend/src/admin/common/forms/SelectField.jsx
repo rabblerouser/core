bc0df5f3b866acb4addf426bd89efae492b4ba87
Dan Mutton <daniel.mutton@gmail.com>
2016-12-18 16:46:18 +1100

    Add support for multi-select

--- frontend/src/admin/common/forms/SelectField.jsx
+++ frontend/src/admin/common/forms/SelectField.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import FieldError from './FieldError';
 
-const SelectField = ({ input, label, type, id, meta: { touched, error }, children }) => (
+const SelectField = ({ input, label, type, id, meta: { touched, error }, children, multiple }) => (
   <div>
     <label htmlFor={input.name} className={(touched && error) ? 'invalid' : ''}>
       {label}
       {touched && error ? <FieldError error={error} /> : ''}
     </label>
-    <select className={id} id={id} {...input} type={type}>
+    <select className={id} id={id} {...input} type={type} multiple={multiple} >
       {children}
     </select>
   </div>
@@ -18,6 +18,7 @@ SelectField.propTypes = {
   label: React.PropTypes.string,
   children: React.PropTypes.any,
   type: React.PropTypes.string,
+  multiple: React.PropTypes.bool,
   meta: React.PropTypes.object.isRequired,
   id: React.PropTypes.string,
 };
