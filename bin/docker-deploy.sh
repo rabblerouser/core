85d185f9944110e85604be14371f736732e8c816
Cam Jackson <camjackson89@gmail.com>
2016-12-23 17:50:56 +1100

    Add docker email to docker deploy script

--- bin/docker-deploy.sh
+++ bin/docker-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
 docker pull rabblerouser/rabblerouser-core
 docker build -t rabblerouser/rabblerouser-core backend
