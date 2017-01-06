9a5bf7c9b740613a1e6964ddbaa1698d47b4f845
Cam Jackson <camjackson89@gmail.com>
2017-01-06 21:08:46 +1100

    Another attempt to fix the pipeline

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -e
 
-sudo apt-get -y purge python-apt-common
-sudo apt-get -y purge python3-apt
-sudo apt-get -y purge python3-dbus
-sudo apt-get -y purge python3-gi
-sudo apt-get -y purge python3-pycurl
-sudo apt-get -y purge python3-software-properties
-sudo apt-get -y purge software-properties-common
-sudo apt-get -y purge unattended-upgrades
-
 sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
-ansible-playbook -i "localhost," -c local provisioning/ci.yml
+sudo ansible-playbook -i "localhost," -c local provisioning/ci.yml
 
 npm install
 npm run build
