5eb689e9b8f4845218bfa7ba65a6457b7329a556
Cam Jackson <camjackson89@gmail.com>
2017-01-06 18:19:05 +1100

    Second attempt to fix CI

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+sudo apt-get purge python-apt-common
+sudo apt-get purge python3-apt
+sudo apt-get purge python3-dbus
+sudo apt-get purge python3-gi
+sudo apt-get purge python3-pycurl
+sudo apt-get purge python3-software-properties
+sudo apt-get purge software-properties-common
+sudo apt-get purge unattended-upgrades
+
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
