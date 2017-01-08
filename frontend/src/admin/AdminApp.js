2c7967351604f6819b2c1f3d3784b12c25266bc5
Dan Mutton <daniel.mutton@gmail.com>
2017-01-08 18:29:36 +1100

    Merged admin and network admin routes into one route with RestrictedTo wrapping the network section

--- frontend/src/admin/AdminApp.js
+++ frontend/src/admin/AdminApp.js
@@ -10,7 +10,6 @@ import Cookies from 'js-cookie';
 import { appStarted } from './actions';
 import reducer from './reducers';
 import AdminDashboard from './AdminDashboard';
-import NetworkAdminDashboard from './NetworkAdminDashboard';
 import './stylesheets/style.scss';
 
 const user = Cookies.getJSON('user');
@@ -30,7 +29,6 @@ render(
     <ThemeProvider theme={theme}>
       <Router history={browserHistory}>
         <Route path="/dashboard" component={AdminDashboard} />
-        <Route path="/dashboard/admin" component={NetworkAdminDashboard} />
       </Router>
     </ThemeProvider>
   </Provider>,
--- frontend/src/admin/AdminDashboard.js
+++ frontend/src/admin/AdminDashboard.js
@@ -3,17 +3,19 @@ import React from 'react';
 import Header from './header';
 import UserMessageView from './UserMessageView';
 import BranchManagement from './branchManagement';
+import NetworkManagement from './networkManagement';
+
 import { RestrictedTo } from './common';
 
 const AdminDashboard = () => (
-  <RestrictedTo type="BRANCH">
-    <div className="admin-container">
-      <Header />
-      <UserMessageView />
-      <BranchManagement />
-    </div>
-  </RestrictedTo>
-
+  <div className="admin-container">
+    <Header />
+    <UserMessageView />
+    <BranchManagement />
+    <RestrictedTo type="SUPER">
+      <NetworkManagement />
+    </RestrictedTo>
+  </div>
 );
 
 export default AdminDashboard;
