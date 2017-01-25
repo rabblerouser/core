b5529b8029605cd7da5524de2eebc785713988bc
Dan Mutton <daniel.mutton@gmail.com>
2017-01-25 20:57:57 +1100

    Fix lint issue

--- frontend/src/admin/branchManagement/branchView/__tests__/BranchDetails-test.js
+++ frontend/src/admin/branchManagement/branchView/__tests__/BranchDetails-test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { BranchDetails } from '../BranchDetails';
-import { Panel } from '../../../common';
 
 describe('BranchDetails', () => {
   let rendered;
