759db1eda39c0ba1401caf1bcb8c41c68ff6921a
Dan Mutton <daniel.mutton@gmail.com>
2017-01-25 21:28:31 +1100

    Update remaining form styles

---
+++ frontend/src/admin/common/forms/Input.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  display: block;
+  font-size: 1em;
+  background-color: #fff;
+  border-color: ${props => props.theme.lightGrey};
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 3px;
+  box-shadow: inset 0 1px 3px ${props => props.theme.lightGrey};
+  box-sizing: border-box;
+  padding: 0.5em;
+  width: 100%;
+
+  &:hover {
+    border-color: ${props => props.theme.primaryLight};
+  }
+
+  &:focus {
+    border-color: ${props => props.theme.primaryColour};
+    outline: none;
+  }
+
+  &:disabled {
+    background-color: ${props => props.theme.lightGrey};
+    cursor: not-allowed;
+
+    &:hover {
+      border: 1px solid ${props => props.theme.lightGrey};
+    }
+  }
+`;
---
+++ frontend/src/admin/common/forms/Label.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.label`
+  display: block;
+  font-size: 1em;
+  padding: 2px;
+`;
