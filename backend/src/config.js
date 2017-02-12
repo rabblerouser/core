14a05ddc6984d589b2f6b85c469d1bd37ad17c4e
Cam Jackson <camjackson89@gmail.com>
2017-02-12 15:05:02 +1100

    Refactor config to remove const mutation

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const config = {
+const baseConfig = {
   email: {
     sendEmails: process.env.SEND_EMAILS,
     transporter: process.env.EMAIL_TRANSPORTER,
@@ -12,28 +12,35 @@ const config = {
     welcomeBody: process.env.EMAIL_WELCOME_BODY || 'Welcome to Rabble Rouser!<br>You can now start participating and getting involved towards the development of a more secure and transparent Australia.\nFor a list of upcoming meetings and discussions, head to rabblerouser.team\nBest,\nRabble Rouser',
   },
   session: {
-    proxy: false,
-    secureCookie: false,
     secret: process.env.SESSION_SECRET || "i'm a teapot",
     domain: process.env.SESSION_DOMAIN,
-  },
-  logFormat: '[:date[iso]] :method :url :status :response-time ms - :req[header]',
-  aws: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'FAKE',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'ALSO FAKE',
-    kinesisEndpoint: 'http://localhost:4567',
   },
   eventAuthToken: process.env.EVENT_AUTH_TOKEN || 'secret',
 };
 
-if (process.env.NODE_ENV === 'production') {
-  config.session.proxy = true;
-  config.session.secureCookie = true;
+const devConfig = Object.assign({}, baseConfig, {
+  session: Object.assign({}, baseConfig.session, {
+    proxy: false,
+    secureCookie: false,
+  }),
+  aws: {
+    accessKeyId: 'FAKE',
+    secretAccessKey: 'ALSO FAKE',
+    kinesisEndpoint: 'http://localhost:4567',
+  },
+  logFormat: '[:date[iso]] :method :url :status :response-time ms - :req[header]',
+});
 
-  // Let the SDK derive the endpoint from the region
-  config.aws.kinesisEndpoint = undefined;
+const prodConfig = Object.assign({}, baseConfig, {
+  session: Object.assign({}, baseConfig.session, {
+    proxy: true,
+    secureCookie: true,
+  }),
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  logFormat: '[:date[iso]] :remote-addr - :req[user] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+});
 
-  config.logFormat = '[:date[iso]] :remote-addr - :req[user] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
-}
-
-module.exports = config;
+module.exports = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
