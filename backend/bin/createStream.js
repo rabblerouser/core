8b15b68186d718e109f58e029d211e8d3a6d1a96
Cam Jackson <camjackson89@gmail.com>
2017-02-20 21:13:11 +1100

    Add workdir to Dockerfile.dev, fix environment variable in createStream script

--- backend/bin/createStream.js
+++ backend/bin/createStream.js
@@ -1,14 +1,14 @@
 const AWS = require('aws-sdk');
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('Not creating stream in production. It should already be there.')
+  console.log('Not creating stream in production. It should already be there.');
   process.exit(0);
 }
 
 console.log('Running createStream script');
 
 const kinesis = new AWS.Kinesis({
-  endpoint: process.env.KINESIS_URL,
+  endpoint: process.env.KINESIS_ENDPOINT,
   region: 'ap-southeast-2',
   accessKeyId: 'FAKE',
   secretAccessKey: 'ALSO FAKE',
@@ -16,5 +16,5 @@ const kinesis = new AWS.Kinesis({
 
 kinesis.createStream({ StreamName: 'rabblerouser_stream', ShardCount: 1 }).promise().then(
   () => console.log('rabblerouser_stream created'),
-  (err) => { throw new Error(`Could not create stream: ${err.message}`) }
+  err => { throw new Error(`Could not create stream: ${err.message}`); }
 );
--- go.sh
+++ go.sh
@@ -4,11 +4,11 @@
 # `./go.sh` to get an interactive shell in a container for development
 # `./go.sh npm test` to run the tests in a container and then exit
 
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 if [[ -z $1 ]];
 then
-  docker-compose exec rabblerouser-core /bin/sh
+  docker-compose exec core /bin/sh
 else
-  docker-compose exec rabblerouser-core "$@"
+  docker-compose exec core "$@"
 fi
