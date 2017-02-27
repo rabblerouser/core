b4b33cdec3e1e11fac8e1f274c72919077edb69e
Cam Jackson <camjackson89@gmail.com>
2017-02-27 12:30:09 +1100

    Merge the createStream and seed scripts

--- backend/bin/seed.js
+++ backend/bin/seed.js
@@ -3,6 +3,7 @@
 // This file runs whenever the backend is about to start.
 // It makes sure that there's at least one network admin and branch in the database
 // This is the minimum data needed for the app to function correctly
+// If we're not in production, it also creates the kinesis stream
 
 if (process.env.NODE_ENV === 'test') {
   console.log('Skipping regular seed script because NODE_ENV=test');
@@ -10,6 +11,7 @@ if (process.env.NODE_ENV === 'test') {
   process.exit(0);
 }
 
+const AWS = require('aws-sdk');
 const models = require('../src/models');
 
 const email = 'networkadmin@rabblerouser.team';
@@ -38,7 +40,32 @@ const branchPromise = models.Branch.findAll()
     return undefined;
   });
 
-Promise.all([adminPromise, branchPromise])
+const createStream = () => {
+  console.log('Creating kinesis stream for development');
+
+  const kinesis = new AWS.Kinesis({
+    endpoint: process.env.KINESIS_ENDPOINT,
+    region: 'ap-southeast-2',
+    accessKeyId: 'FAKE',
+    secretAccessKey: 'ALSO FAKE',
+  });
+
+  return kinesis.createStream({ StreamName: 'rabblerouser_stream', ShardCount: 1 }).promise().then(
+    () => console.log('rabblerouser_stream created'),
+    err => {
+      // Swallow these errors, but re-throw all others
+      if (err.message.includes('already exists')) {
+        console.log('Stream already exists');
+        return;
+      }
+      throw new Error(`Could not create stream: ${err.message}`);
+    }
+  );
+};
+
+const kinesisPromise = process.env.NODE_ENV === 'production' ? Promise.resolve() : createStream();
+
+Promise.all([adminPromise, branchPromise, kinesisPromise])
   .then(() => console.log('Seeded successfully.'))
   .catch(error => {
     console.error('Seed failed:', error);
