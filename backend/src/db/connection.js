3e594e62215267da74836efb5a4b9869e83d7f62
Cam Jackson <camjackson89@gmail.com>
2017-03-01 22:57:23 +1100

    Add a separate postgresql instance to run tests against, so that `npm test` doesn't blow away your development DB

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -6,12 +6,4 @@ const cls = require('continuation-local-storage');
 
 Sequelize.cls = cls.createNamespace('rabblerouser');
 
-let connection;
-
-if (config.use_env_variable) {
-  connection = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  connection = new Sequelize(config.database, config.username, config.password, config);
-}
-
-module.exports = connection;
+module.exports = new Sequelize(process.env[config.use_env_variable], config);
