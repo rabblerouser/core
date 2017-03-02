dea47bf0bf38b3fc712ce7869551a12ca8ea09d0
Cam Jackson <camjackson89@gmail.com>
2017-03-02 17:21:29 +1100

    Change the CI script to use the dockerised dev environment

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,13 +2,6 @@
 set -e
 set -x
 
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-
-sudo ansible-playbook -i "localhost," -c local provisioning/ci.yml
-
-npm install
-npm run build
-npm test
+./go.sh npm install
+./go.sh npm run build
+./go.sh npm test
