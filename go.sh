5dd3b48d9562148f66a8054c22a60a502c595a30
Cam Jackson <camjackson89@gmail.com>
2017-03-03 10:36:09 +1100

    Give up on circleci for now, and revert some changes to ./go.sh

--- go.sh
+++ go.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
 # Usage examples:
 # `./go.sh` to get an interactive shell in a container for development
 # `./go.sh npm test` to run the tests in a container and then exit
 
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 if [[ -z $1 ]];
 then
