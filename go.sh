2f7f7d58ffb97049f1f50d7bf728b969c9e6d27e
Cam Jackson <camjackson89@gmail.com>
2017-03-03 10:13:54 +1100

    Take out the --remove-orphans flag from go.sh because docker-compose on snap seems to struggle with it

--- go.sh
+++ go.sh
@@ -7,7 +7,7 @@ set -x
 # `./go.sh` to get an interactive shell in a container for development
 # `./go.sh npm test` to run the tests in a container and then exit
 
-docker-compose up -d --remove-orphans
+docker-compose up -d
 
 if [[ -z $1 ]];
 then
