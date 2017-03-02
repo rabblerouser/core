b07327810225f0fe391ad8313cea0cc00297e08d
Cam Jackson <camjackson89@gmail.com>
2017-03-03 09:29:40 +1100

    Add some debug logging to .go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 # Usage examples:
 # `./go.sh` to get an interactive shell in a container for development
 # `./go.sh npm test` to run the tests in a container and then exit
