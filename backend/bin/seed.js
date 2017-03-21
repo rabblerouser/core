bfde8bf0ee02953ec3fc034e53b6329f3e1d05ce
Cam Jackson <camjackson89@gmail.com>
2017-03-21 21:39:45 +1100

    Make seeding an explicit step, rather than a pre-start script. Seed the default branch via kinesis, not postgres.

--- backend/bin/seed.js
+++ backend/bin/seed.js
@@ -3,45 +3,31 @@
 /* eslint no-console: off */
 /* eslint import/no-extraneous-dependencies: off */
 
-// This file runs whenever the backend is about to start.
-// It makes sure that there's at least one network admin and branch in the database
-// This is the minimum data needed for the app to function correctly
-// If we're not in production, it also creates the kinesis stream and the event archive S3 bucket
-
-if (process.env.NODE_ENV === 'test') {
-  console.log('Skipping regular seed script because NODE_ENV=test');
-  console.log('Any necessary test data should be set up by a different script');
-  process.exit(0);
-}
+// This script can be run manually via npm to prepare data for the app
+// - It inserts an Admin user into the relational DB directly
+// - It publishes a "branch-created" event onto the rabblerouser kinesis stream
+// - In development, it also ensures that the kinesis stream and event S3 bucket have been created
+//   (In production, these are created by terraform before the app is deployed)
 
 const AWS = require('aws-sdk');
+const branchesController = require('../src/controllers/branchesController');
 const models = require('../src/models');
 
 const email = 'networkadmin@rabblerouser.team';
 const password = 'password';
 const type = 'SUPER';
 
-console.log('Running seed script');
-
-const adminPromise = models.AdminUser.findAll()
-  .then(adminUsers => {
-    if (adminUsers.length === 0) {
-      console.log(`No network admins exist, creating one - Username: ${email}, password: ${password}`);
-      return models.AdminUser.create({ email, password, type });
-    }
-    console.log('At least one network admin already exists.');
-    return undefined;
-  });
-
-const branchPromise = models.Branch.findAll()
-  .then(branches => {
-    if (branches.length === 0) {
-      console.log('No branches exist, creating one - ');
-      return models.Branch.create({ name: 'Default branch' });
-    }
-    console.log('At least one branch already exists.');
-    return undefined;
-  });
+const createAdmin = () => (
+  models.AdminUser.findAll()
+    .then(adminUsers => {
+      if (adminUsers.length === 0) {
+        console.log(`No network admins exist, creating one - Username: ${email}, password: ${password}`);
+        return models.AdminUser.create({ email, password, type });
+      }
+      console.log('At least one network admin already exists.');
+      return undefined;
+    })
+);
 
 const createStream = () => {
   console.log('Creating kinesis stream for development');
@@ -83,10 +69,51 @@ const createArchiveBucket = () => {
   );
 };
 
-const kinesisPromise = process.env.NODE_ENV === 'production' ? Promise.resolve() : createStream();
-const s3Promise = process.env.NODE_ENV === 'production' ? Promise.resolve() : createArchiveBucket();
+const createStreamAndBucket = () => {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('Not creating kinesis stream or S3 bucket. These should already be there in production.');
+    return undefined;
+  }
+  return Promise.all([createStream(), createArchiveBucket()]);
+};
 
-Promise.all([adminPromise, branchPromise, kinesisPromise, s3Promise])
+const fakeRes = () => {
+  const res = {
+    lastStatus: null,
+    lastJson: null,
+  };
+  res.status = status => { res.lastStatus = status; return res; };
+  res.sendStatus = status => { res.lastStatus = status; return res; };
+  res.json = json => { res.lastJson = json; return res; };
+  return res;
+};
+
+const createDefaultBranch = () => {
+  console.log('Creating default branch.');
+  const req = { body: { name: 'Default branch' } };
+  const res = fakeRes();
+  return branchesController.createBranch(req, res)
+    .then(() => {
+      if (res.lastStatus === 200) {
+        console.log('Default branch created');
+        return Promise.resolve();
+      }
+      console.log('Failed to create default branch');
+      return Promise.reject({ status: res.lastStatus, json: res.lastJson });
+    });
+};
+
+const wait = () => {
+  console.log('Waiting for a few seconds for things to be ready...');
+  return new Promise(resolve => setTimeout(resolve, 5000));
+};
+
+console.log('Running seed script');
+Promise.resolve()
+  .then(createAdmin)
+  .then(createStreamAndBucket)
+  .then(wait)
+  .then(createDefaultBranch)
   .then(() => console.log('Seeded successfully.'))
   .catch(error => {
     console.error('Seed failed:', error);
