098efc7d035f07165ac9b85c7c248c8e7e8df166
Cam Jackson <camjackson89@gmail.com>
2017-03-30 22:08:42 +1100

    Convert the passport strategy to use the in-memory admin store rather than the database

--- backend/src/security/passport.js
+++ backend/src/security/passport.js
@@ -2,28 +2,35 @@
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const AdminUser = require('../models/index').AdminUser;
+const bcrypt = require('bcryptjs');
 const logger = require('../lib/logger');
+const store = require('../store');
+
+const authenticate = (email, password, done) => {
+  const user = store.getAdmins().find(admin => admin.email === email);
+  if (user && bcrypt.compareSync(password, user.password)) {
+    logger.info('admin-logged-in', email);
+    done(null, user);
+  } else {
+    logger.info('admin-failed-log-in', email);
+    done(null, false);
+  }
+};
 
 passport.use(new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password',
-}, AdminUser.authenticate));
+}, authenticate));
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => (
-  AdminUser.findById(id)
-    .then(dbResult => ({
-      email: dbResult.email,
-      branchId: dbResult.branchId,
-      type: dbResult.type,
-    }))
-    .nodeify(done)
-    .catch(err => {
-      logger.error('failed to deserialize', err.toString());
-      done(null, false);
-    })
-));
+passport.deserializeUser((id, done) => {
+  const user = store.getAdmins().find(admin => admin.id === id);
+  if (user) {
+    return done(null, user);
+  }
+  logger.error('failed to deserialize', id);
+  return done(null, false);
+});
