ef8247786fc9956a9ab2ec69b7378a8f4bdc35ab
Cam Jackson <camjackson89@gmail.com>
2017-04-01 11:53:08 +1100

    Remove branch id validation from the member controller now that we have middleware for it

--- backend/src/lib/memberValidator.js
+++ backend/src/lib/memberValidator.js
@@ -3,14 +3,10 @@
 const inputValidator = require('./inputValidator');
 const _ = require('lodash');
 
-const isValidBranch = name =>
-  inputValidator.isValidString(name, new RegExp('[<>"%;&+]')) && name.length < 256;
-
 const memberFieldsChecks = {
   email: inputValidator.isValidEmail,
   phoneNumber: inputValidator.isValidOptionalPhoneNumber,
   name: inputValidator.isValidName,
-  branchId: isValidBranch,
   additionalInfo: inputValidator.isValidOptionalTextBlock,
   notes: inputValidator.isValidOptionalTextBlock,
 };
