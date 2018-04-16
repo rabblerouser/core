b7cfbd67cc83a75749a7b0834499e29d1ac30448
Para O'Kelly <hksokelly@gmail.com>
2018-04-16 16:17:11 +0800

    Updated travis config; Changed spin up script; Removed user from docker-compose config

--- go.sh
+++ go.sh
@@ -5,12 +5,11 @@ set -e
 # Usage examples:
 # `./go.sh` to get an interactive shell in a container for development
 # `./go.sh yarn test` to run the tests in a container and then exit
-
 docker-compose up -d --remove-orphans
 
 if [[ -z $1 ]];
 then
-  docker-compose exec core /bin/sh
+  docker-compose exec core /bin/bash
 else
   docker-compose exec core "$@"
 fi
