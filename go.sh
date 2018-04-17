d363e037811c5659f40fba9e0266397ddb430929
Para O'Kelly <hksokelly@gmail.com>
2018-04-17 13:37:51 +0800

    Edited go script to also include e2e test runner

--- go.sh
+++ go.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# This follows a different path to the other tests/shell, since the e2e script
+# spins up its own containers as needed.
+if [ $1 = "e2e" ]
+then
+  ./bin/e2e.sh
+  exit
+fi
+
 # Usage examples:
 # `./go.sh` to get an interactive shell in a container for development
 # `./go.sh yarn test` to run the tests in a container and then exit
