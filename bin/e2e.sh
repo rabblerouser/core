038eab595e9f167487eb7195cf926868f613dd15
Amelyn Wongso <awongso@thoughtworks.com>
2018-04-30 15:02:46 +0800

    Make log messages appear for core_test_1 and core_core_1

--- bin/e2e.sh
+++ bin/e2e.sh
@@ -3,4 +3,5 @@ set -e
 
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d
 
-docker logs -f --since=1s core_test_1
+docker logs -f --since=1m core_test_1
+docker logs -f --since=1m core_core_1
