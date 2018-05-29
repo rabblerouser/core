5ac6433088997ff09d81a19f9397db645f257d52
Amelyn Wongso <amelynwongso@AUawongso.local>
2018-05-29 17:04:55 +0800

    Revert "Remove tests temporarily."
    This reverts commit 07e0795a11f76a0af5a033d37bc7253aab1db803.

--- e2e/cypress/integration/admin_journey.js
+++ e2e/cypress/integration/admin_journey.js
@@ -8,8 +8,28 @@ import {
 } from './pages/adminPage';
 
 describe('Login page', function () {
-  it('greets with Rabble Rouser Dashboard Login', function () {
+  beforeEach(() => {
     startAtLogin();
+  })
+
+  it('greets with Rabble Rouser Dashboard Login', function () {
     title().should('include', 'Rabble Rouser - Login');
   });
+
+  it('stays on the Login page on unsuccessful login', function () {
+    login('orgnsr@thebranch.org', 'bad password');
+
+    title().should('include', 'Rabble Rouser - Login');
+  });
+
+  it('navigates to dashboard page on successful login', function () {
+    login('admin@rr.com', 'password4321');
+    cy.url().should('include', 'dashboard');
+  });
 });
+
+function login(email, password) {
+  return enterEmail(email)
+    .then(() => enterPassword(password))
+    .then(clickLogin);
+}
